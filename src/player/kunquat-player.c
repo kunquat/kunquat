5af59695407b44d8e4eafaa2e99c0b5c1b5a6b26
Tomi Jylhä-Ollila <tomi.jylha-ollila@helsinki.fi>
2009-07-04 16:28:34 +0300

    Added terminal handling code into kunquat-player.

--- src/player/kunquat-player.c
+++ src/player/kunquat-player.c
@@ -32,6 +32,11 @@
 
 #include <getopt.h>
 
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
 #include <Audio.h>
 
 #include <kqt_Context.h>
@@ -73,6 +78,8 @@ void usage(void)
         fprintf(stderr, ", %s", driver_names[i]);
     }
     fprintf(stderr, "\n");
+    fprintf(stderr, "   -q, --quiet                Quiet and non-interactive operation\n");
+    fprintf(stderr, "                              (only error messages will be displayed)\n");
     return;
 }
 
@@ -92,6 +99,105 @@ void get_minutes_seconds(uint64_t frames, uint32_t freq, int* minutes, double* s
 }
 
 
+bool set_terminal(bool interactive, bool immediate)
+{
+    struct termios* term = &(struct termios){ .c_lflag = 0 };
+    errno = 0;
+    if (tcgetattr(STDIN_FILENO, term) != 0)
+    {
+        perror("Couldn't retrieve terminal attributes");
+        return false;
+    }
+    if (interactive)
+    {
+        term->c_lflag &= ~ICANON;
+        term->c_lflag &= ~ECHO;
+    }
+    else
+    {
+        term->c_lflag |= ICANON;
+        term->c_lflag |= ECHO;
+    }
+    errno = 0;
+    if (tcsetattr(STDIN_FILENO, TCSANOW, term))
+    {
+        perror("Couldn't set terminal attributes");
+        return false;
+    }
+    errno = 0;
+    int flags = fcntl(STDIN_FILENO, F_GETFL);
+    if (flags == -1)
+    {
+        perror("Couldn't retrieve standard input attributes");
+        return false;
+    }
+    if (immediate)
+    {
+        flags |= O_NONBLOCK;
+    }
+    else
+    {
+        flags &= ~O_NONBLOCK;
+    }
+    if (fcntl(STDIN_FILENO, F_SETFL, flags) == -1)
+    {
+        perror("Couldn't set standard input attributes");
+        return false;
+    }
+    return true;
+}
+
+
+typedef enum
+{
+    KEY_NONE = 0,
+    KEY_LEFT,
+    KEY_DOWN,
+    KEY_UP,
+    KEY_RIGHT
+} Key;
+
+
+int get_key(void)
+{
+    char key = 0;
+    if (fread(&key, 1, 1, stdin) == 0)
+    {
+        return -1;
+    }
+    if (key == '\033')
+    {
+        char keys[2] = { '\0' };
+        int ret = fread(keys, 1, 2, stdin);
+        if (ret < 2)
+        {
+            return -1;
+        }
+        if (keys[0] == '[')
+        {
+            if (keys[1] == 'A')
+            {
+                return KEY_UP;
+            }
+            else if (keys[1] == 'B')
+            {
+                return KEY_DOWN;
+            }
+            else if (keys[1] == 'C')
+            {
+                return KEY_RIGHT;
+            }
+            else if (keys[1] == 'D')
+            {
+                return KEY_LEFT;
+            }
+        }
+        return -1;
+    }
+    return key;
+}
+
+
 int main(int argc, char** argv)
 {
     if (argc < 2)
@@ -99,16 +205,20 @@ int main(int argc, char** argv)
         usage();
         exit(EXIT_FAILURE);
     }
+
     char* driver_selection = driver_names[0];
+    bool interactive = true;
+
     struct option long_options[] =
     {
         { "help", no_argument, NULL, 'h' },
         { "driver", required_argument, NULL, 'd' },
+        { "quiet", no_argument, NULL, 'q' },
         { NULL, 0, NULL, 0 }
     };
     int opt = 0;
     int opt_index = 1;
-    while ((opt = getopt_long(argc, argv, ":hd:", long_options, &opt_index)) != -1)
+    while ((opt = getopt_long(argc, argv, ":hd:q", long_options, &opt_index)) != -1)
     {
         switch (opt)
         {
@@ -121,6 +231,11 @@ int main(int argc, char** argv)
             case 'd':
             {
                 driver_selection = optarg;
+            }
+            break;
+            case 'q':
+            {
+                interactive = false;
             }
             break;
             case ':':
@@ -150,6 +265,12 @@ int main(int argc, char** argv)
         fprintf(stderr, "No input files.\n");
         exit(EXIT_SUCCESS);
     }
+    
+    if (interactive && !set_terminal(true, true))
+    {
+        fprintf(stderr, "Couldn't set terminal attributes\n");
+    }
+
     Audio* audio = new_Audio(driver_selection);
     if (audio == NULL)
     {
@@ -162,7 +283,8 @@ int main(int argc, char** argv)
         del_Audio(audio);
         exit(EXIT_FAILURE);
     }
-    for (int file_arg = optind; file_arg < argc; ++file_arg)
+    bool quit = false;
+    for (int file_arg = optind; file_arg < argc && !quit; ++file_arg)
     {
         kqt_Error* error = KQT_ERROR_AUTO;
         kqt_Context* context = kqt_new_Context_from_path(argv[file_arg],
@@ -190,46 +312,35 @@ int main(int argc, char** argv)
         kqt_Mix_state* mix_state = kqt_Mix_state_init(KQT_MIX_STATE_AUTO);
         Audio_get_state(audio, mix_state);
         uint16_t max_voices = 0;
-        fprintf(stderr, "Playing %s\n", argv[file_arg]);
+        if (interactive)
+        {
+            fprintf(stderr, "Playing %s\n", argv[file_arg]);
+        }
         while (mix_state->playing)
         {
-            uint32_t frames = mix_state->frames;
-            int status_line_pos = 0;
-            int minutes = 0;
-            double seconds = 0;
-            get_minutes_seconds(frames, freq, &minutes, &seconds);
-            uint64_t frames_left = length_frames - frames;
-            int minutes_left = 0;
-            double seconds_left = 0;
-            get_minutes_seconds(frames_left, freq, &minutes_left, &seconds_left);
-            if (mix_state->voices > max_voices)
+            if (interactive)
             {
-                max_voices = mix_state->voices;
-            }
-            double pos = kqt_Reltime_get_beats(&mix_state->pos) +
-                         ((double)kqt_Reltime_get_rem(&mix_state->pos) / KQT_RELTIME_BEAT);
-            int printed = snprintf(status_line + status_line_pos,
-                                   status_line_max - status_line_pos,
-                                   "Subsong: %02" PRIu16
-                                   ", Time: %02d:%04.1f",
-                                   mix_state->subsong,
-                                   minutes, seconds);
-            if (printed >= 0)
-            {
-                status_line_pos += printed;
-                if (status_line_pos >= status_line_max)
+                uint32_t frames = mix_state->frames;
+                int status_line_pos = 0;
+                int minutes = 0;
+                double seconds = 0;
+                get_minutes_seconds(frames, freq, &minutes, &seconds);
+                uint64_t frames_left = length_frames - frames;
+                int minutes_left = 0;
+                double seconds_left = 0;
+                get_minutes_seconds(frames_left, freq, &minutes_left, &seconds_left);
+                if (mix_state->voices > max_voices)
                 {
-                    status_line_pos = status_line_max - 1;
+                    max_voices = mix_state->voices;
                 }
-            }
-            if (length_frames >= mix_state->frames)
-            {
-                printed = snprintf(status_line + status_line_pos,
-                                   status_line_max - status_line_pos,
-                                   " [%02d:%04.1f]"
-                                   " of %02d:%04.1f",
-                                   minutes_left, seconds_left,
-                                   minutes_total, seconds_total);
+                double pos = kqt_Reltime_get_beats(&mix_state->pos) +
+                             ((double)kqt_Reltime_get_rem(&mix_state->pos) / KQT_RELTIME_BEAT);
+                int printed = snprintf(status_line + status_line_pos,
+                                       status_line_max - status_line_pos,
+                                       "Subsong: %02" PRIu16
+                                       ", Time: %02d:%04.1f",
+                                       mix_state->subsong,
+                                       minutes, seconds);
                 if (printed >= 0)
                 {
                     status_line_pos += printed;
@@ -238,43 +349,79 @@ int main(int argc, char** argv)
                         status_line_pos = status_line_max - 1;
                     }
                 }
-            }
-            printed = snprintf(status_line + status_line_pos,
-                               status_line_max - status_line_pos,
-                               ", Position: %02" PRIu16 "/%04.1f"
-                               ", Voices: %" PRIu16 " (%" PRIu16 ")",
-                               mix_state->order, pos,
-                               mix_state->voices, max_voices);
-            if (printed >= 0)
-            {
-                status_line_pos += printed;
-                if (status_line_pos >= status_line_max)
+                if (length_frames >= mix_state->frames)
                 {
-                    status_line_pos = status_line_max - 1;
+                    printed = snprintf(status_line + status_line_pos,
+                                       status_line_max - status_line_pos,
+                                       " [%02d:%04.1f]"
+                                       " of %02d:%04.1f",
+                                       minutes_left, seconds_left,
+                                       minutes_total, seconds_total);
+                    if (printed >= 0)
+                    {
+                        status_line_pos += printed;
+                        if (status_line_pos >= status_line_max)
+                        {
+                            status_line_pos = status_line_max - 1;
+                        }
+                    }
+                }
+                printed = snprintf(status_line + status_line_pos,
+                                   status_line_max - status_line_pos,
+                                   ", Position: %02" PRIu16 "/%04.1f"
+                                   ", Voices: %" PRIu16 " (%" PRIu16 ")",
+                                   mix_state->order, pos,
+                                   mix_state->voices, max_voices);
+                if (printed >= 0)
+                {
+                    status_line_pos += printed;
+                    if (status_line_pos >= status_line_max)
+                    {
+                        status_line_pos = status_line_max - 1;
+                    }
+                }
+                if (status_line_pos >= status_line_bytes_used)
+                {
+                    status_line_bytes_used = status_line_pos;
+                }
+                else
+                {
+                    for (int i = status_line_pos; i < status_line_bytes_used; ++i)
+                    {
+                        status_line[i] = ' ';
+                    }
+                    status_line[status_line_bytes_used] = '\0';
+                }
+                fprintf(stderr, "%s\r", status_line);
+             
+                int key = get_key();
+                if (key == 'q')
+                {
+                    quit = true;
+                    break;
                 }
             }
-            if (status_line_pos >= status_line_bytes_used)
-            {
-                status_line_bytes_used = status_line_pos;
-            }
-            else
-            {
-                for (int i = status_line_pos; i < status_line_bytes_used; ++i)
-                {
-                    status_line[i] = ' ';
-                }
-                status_line[status_line_bytes_used] = '\0';
-            }
-            fprintf(stderr, "%s\r", status_line);
+
             Audio_get_state(audio, mix_state);
         }
-        fprintf(stderr, "\n");
+        if (interactive)
+        {
+            fprintf(stderr, "\n");
+        }
         Audio_set_context(audio, NULL);
         Audio_get_state(audio, mix_state);
         kqt_del_Context(context);
     }
-    fprintf(stderr, "Done.\n");
+    if (interactive)
+    {
+        fprintf(stderr, "Done.\n");
+    }
     del_Audio(audio);
+    if (interactive && !set_terminal(false, false))
+    {
+        fprintf(stderr, "Couldn't reset terminal attributes"
+                        " -- you may have to run `reset`.\n");
+    }
     return EXIT_SUCCESS;
 }
 
