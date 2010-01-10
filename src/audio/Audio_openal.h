9bc83075b4b3f2a420d9d2c2876f6cd13c06b5ce
Tomi Jylhä-Ollila <tomi.jylha-ollila@helsinki.fi>
2010-01-10 03:11:54 +0200

    Several small documentation fixes.

--- src/audio/Audio_openal.h
+++ src/audio/Audio_openal.h
@@ -1,16 +1,16 @@
 
 
 /*
- * Authors: Heikki Aitakangas, Finland, 2009
- *          Tomi Jylhä-Ollila, Finland, 2010
+ * Authors: Heikki Aitakangas, Finland 2009
+ *          Tomi Jylhä-Ollila, Finland 2010
  *
  * This file is part of Kunquat.
  *
  * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
  *
- * To the extent possible under law, Kunquat waivers have waived all
+ * To the extent possible under law, Kunquat Affirmers have waived all
  * copyright and related or neighboring rights to Kunquat. This work
- * is published from Finland.
+ * is published from various territories.
  */
 
 
