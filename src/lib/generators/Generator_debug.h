d69548e49ba7247c94079da67ad5e70197984024
Tomi Jylhä-Ollila <tomi.jylha-ollila@helsinki.fi>
2010-07-18 20:17:32 +0300

    Removed the old Generator type file.

--- src/lib/generators/Generator_debug.h
+++ src/lib/generators/Generator_debug.h
@@ -31,6 +31,10 @@ typedef struct Generator_debug
 /**
  * Creates a new Debug Generator.
  *
+ * The Debug Generator generates a narrow pulse wave (with one sample value 1,
+ * the rest are 0.5) that lasts no more than 10 phase cycles. Note off lasts
+ * no more than two phase cycles with all sample values negated.
+ *
  * \param buffer_size   The mixing buffer size -- must be > \c 0 and
  *                      <= \c KQT_BUFFER_SIZE_MAX.
  * \param mix_rate      The mixing rate -- must be > \c 0.
