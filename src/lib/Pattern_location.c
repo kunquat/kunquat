f036af0da4663eea987df6de1637dff5e4af1d0f
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2011-01-05 21:27:51 +0200

    Fixed author and copyright information in Pattern_location.c.

--- src/lib/Pattern_location.c
+++ src/lib/Pattern_location.c
@@ -1,5 +1,17 @@
 
 
+/*
+ * Author: Tomi Jylhä-Ollila, Finland 2011
+ *
+ * This file is part of Kunquat.
+ *
+ * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * To the extent possible under law, Kunquat Affirmers have waived all
+ * copyright and related or neighboring rights to Kunquat.
+ */
+
+
 #include <stdlib.h>
 
 #include <kunquat/limits.h>
