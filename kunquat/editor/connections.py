48719ce3fb6f342b787a772f1cfdef1ecc348bc2
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2011-01-27 17:54:40 +0200

    Rewrote the connection editor widget.

--- kunquat/editor/connections.py
+++ kunquat/editor/connections.py
@@ -30,98 +30,40 @@ class Connections(QtGui.QWidget):
         layout.setMargin(0)
         layout.setSpacing(0)
 
-        self._list_area = CListArea(project, key)
-        layout.addWidget(self._list_area)
+        self._list = CList(project, key)
+        layout.addWidget(self._list)
 
         self.sync()
-
-    def sync(self):
-        self._list_area.sync()
-
-
-class CListArea(QtGui.QScrollArea):
-
-    def __init__(self, project, key, parent=None):
-        QtGui.QScrollArea.__init__(self, parent)
-        self._project = project
-        self._list = CList(project, key)
-        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.setWidget(self._list)
-        self.setWidgetResizable(True)
 
     def sync(self):
         self._list.sync()
 
 
-class CList(QtGui.QWidget):
+class CList(QtGui.QTableWidget):
 
     def __init__(self, project, key, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        self.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
-                           QtGui.QSizePolicy.MinimumExpanding)
+        QtGui.QTableWidget.__init__(self, 2, 2, parent)
         self._project = project
         self._key = key
-        self._layout = QtGui.QVBoxLayout(self)
-        self._layout.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
-        self._layout.addWidget(Connection(key), 1)
+        self.setHorizontalHeaderLabels(['Source', 'Destination'])
+        self.horizontalHeader().setStretchLastSection(True)
+        self.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.verticalHeader().hide()
 
     def sync(self):
         conns = self._project[self._key]
         if not conns:
-            for i in xrange(len(self._layout)):
-                item = self._layout.itemAt(i)
-                item.widget().edge = None
+            for col, row in ((a, b) for a in xrange(self.columnCount())
+                                    for b in xrange(self.rowCount())):
+                item = QtGui.QTableWidgetItem()
+                self.setItem(row, col, item)
             return
-        add_count = len(conns) + 1 - len(self._layout)
-        if add_count > 0:
-            for _ in xrange(add_count):
-                self._layout.addWidget(Connection(self._key), 1)
+        new_rows = len(conns) + 1 - self.rowCount()
+        for _ in xrange(new_rows):
+            self.insertRow(self.rowCount())
         for i, conn in enumerate(conns):
-            item = self._layout.itemAt(i)
-            item.widget().edge = conn
-
-
-class Connection(QtGui.QWidget):
-
-    def __init__(self, key, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        self.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
-                           QtGui.QSizePolicy.MinimumExpanding)
-        layout = QtGui.QHBoxLayout(self)
-        layout.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
-        self._src = QtGui.QLineEdit()
-        self._src.setValidator(ConnValidator(key))
-        layout.addWidget(self._src, 1)
-        self._dest = QtGui.QLineEdit()
-        self._dest.setValidator(ConnValidator(key))
-        layout.addWidget(self._dest, 1)
-
-    @property
-    def edge(self):
-        src = self._src.text()
-        dest = self._dest.text()
-        if not (src and dest):
-            return None
-        return [src, dest]
-
-    @edge.setter
-    def edge(self, pair):
-        if not pair:
-            self._src.setText('')
-            self._dest.setText('')
-            return
-        src, dest = pair
-        self._src.setText(src)
-        self._dest.setText(dest)
-
-
-class ConnValidator(QtGui.QValidator):
-
-    def __init__(self, key):
-        super(ConnValidator, self).__init__()
-        self._key = key
-
-    def validate(self, field, pos):
-        return QtGui.QValidator.Acceptable, pos
+            src, dest = conn
+            self.setItem(i, 0, QtGui.QTableWidgetItem(src))
+            self.setItem(i, 1, QtGui.QTableWidgetItem(dest))
 
 
