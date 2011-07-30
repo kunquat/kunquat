6e4fb0275491f2974c43f8b8f219a0f90ee4c5b3
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2011-07-31 00:37:17 +0300

    Added more stub widgets for sample maps.

--- kunquat/editor/instruments/sample_map.py
+++ kunquat/editor/instruments/sample_map.py
@@ -15,17 +15,72 @@ from __future__ import division, print_function
 
 from PyQt4 import QtGui, QtCore
 
+import kunquat.editor.kqt_limits as lim
 from kunquat.editor.plane_view import PlaneView, HAxis, VAxis
 
 
-class SampleMap(PlaneView):
+RANDOMS_MAX = 8
+SAMPLES_MAX = 512
 
-    keyChanged = QtCore.pyqtSignal(float, float, name='keyChanged')
+
+class SampleMap(QtGui.QWidget):
 
     def __init__(self, project, key, parent=None):
-        PlaneView.__init__(self, parent)
+        QtGui.QWidget.__init__(self, parent)
         self._project = project
         self._key = key
+        self._map = {}
+        layout = QtGui.QHBoxLayout(self)
+        layout.setMargin(0)
+        layout.setSpacing(0)
+        self._map_view = MapView()
+        random_layout = QtGui.QVBoxLayout()
+        for _ in xrange(RANDOMS_MAX):
+            random_layout.addWidget(Entry())
+        layout.addWidget(self._map_view)
+        layout.addLayout(random_layout)
+
+    def set_key(self, key):
+        self._key = key
+        self._map = {}
+        if not key:
+            return
+        map_list = self._project[key]
+        for point, rand_list in map_list:
+            self._map[tuple(point)] = rand_list
+        self._map_view.set_data(self._map)
+
+    def sync(self):
+        self.set_key(self._key)
+
+
+class Entry(QtGui.QWidget):
+
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        layout = QtGui.QHBoxLayout(self)
+        layout.setMargin(0)
+        layout.setSpacing(0)
+        self._freq = QtGui.QSpinBox()
+        self._volume = QtGui.QDoubleSpinBox()
+        self._volume.setDecimals(2)
+        self._sample = QtGui.QSpinBox()
+        self._sample.setMaximum(SAMPLES_MAX - 1)
+        layout.addWidget(self._freq)
+        layout.addWidget(self._volume)
+        layout.addWidget(self._sample)
+
+    def set_data(self, data):
+        pass
+
+
+class MapView(PlaneView):
+
+    activeChanged = QtCore.pyqtSignal(float, float, name='activeChanged')
+
+    def __init__(self, parent=None):
+        PlaneView.__init__(self, parent)
+        self._map = {}
         self._aspect = None
         self._colours = {
                             'bg': QtGui.QColor(0, 0, 0),
@@ -64,15 +119,21 @@ class SampleMap(PlaneView):
         paint.eraseRect(ev.rect())
         self._haxis.paint(paint)
         self._vaxis.paint(paint)
+        self._paint_nodes(paint)
         paint.end()
 
-    def set_key(self, key):
-        self._key = key
-        if not key:
-            return
+    def set_data(self, data):
+        self._map = data
 
-    def sync(self):
-        self.set_key(self._key)
+    def _paint_nodes(self, paint):
+        paint.setPen(QtCore.Qt.NoPen)
+        paint.setBrush(self._colours['node'])
+        diameter = 5
+        for y, x in self._map.iterkeys():
+            rect_x = self._view_x(x) - diameter / 2 + 0.5
+            rect_y = self._view_y(y) - diameter / 2 + 0.5
+            rect = QtCore.QRectF(rect_x, rect_y, diameter, diameter)
+            paint.drawEllipse(rect)
 
     def _set_view(self):
         self._layout['offset'][0] = (-self._layout['visible_min'][0] /
