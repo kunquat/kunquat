620bac65314dceb282af897baaecafb3e9896292
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2012-01-14 21:51:03 +0200

    Separated expression evaluation code from the condition event code.

---
+++ src/lib/Value.h
@@ -0,0 +1,56 @@
+
+
+/*
+ * Author: Tomi Jylhä-Ollila, Finland 2012
+ *
+ * This file is part of Kunquat.
+ *
+ * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * To the extent possible under law, Kunquat Affirmers have waived all
+ * copyright and related or neighboring rights to Kunquat.
+ */
+
+
+#ifndef K_VALUE_H
+#define K_VALUE_H
+
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <Real.h>
+#include <Reltime.h>
+
+
+typedef enum
+{
+    VALUE_TYPE_NONE = 0,
+    VALUE_TYPE_BOOL,
+    VALUE_TYPE_INT,
+    VALUE_TYPE_FLOAT,
+    VALUE_TYPE_REAL,
+    VALUE_TYPE_TIMESTAMP,
+} Value_type;
+
+
+typedef struct Value
+{
+    Value_type type;
+    union
+    {
+        bool bool_type;
+        int64_t int_type;
+        double float_type;
+        Real Real_type;
+        Reltime Timestamp_type;
+    } value;
+} Value;
+
+
+#define VALUE_AUTO (&(Value){ .type = VALUE_TYPE_NONE })
+
+
+#endif // K_VALUE_H
+
+
---
+++ src/lib/expr.h
@@ -0,0 +1,43 @@
+
+
+/*
+ * Author: Tomi Jylhä-Ollila, Finland 2012
+ *
+ * This file is part of Kunquat.
+ *
+ * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * To the extent possible under law, Kunquat Affirmers have waived all
+ * copyright and related or neighboring rights to Kunquat.
+ */
+
+
+#ifndef K_EXPR_H
+#define K_EXPR_H
+
+
+#include <Environment.h>
+#include <File_base.h>
+#include <Value.h>
+
+
+/**
+ * Evaluates an expression.
+ *
+ * \param str     The expression -- must not be \c NULL.
+ * \param env     The Environment -- must not be \c NULL.
+ * \param state   The Read state -- must not be \c NULL.
+ * \param res     A memory location for the result Value --
+ *                must not be \c NULL.
+ *
+ * \return   The position of \a str after parsing.
+ */
+char* evaluate_expr(char* str,
+                    Environment* env,
+                    Read_state* state,
+                    Value* res);
+
+
+#endif // K_EXPR_H
+
+
