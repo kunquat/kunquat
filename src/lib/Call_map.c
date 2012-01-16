ba9dc21e6da490d672237856413a2890228891cb
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2012-01-17 01:20:20 +0200

    Added some more call map code (does not compile).

--- src/lib/Call_map.c
+++ src/lib/Call_map.c
@@ -30,16 +30,68 @@ struct Call_map
 };
 
 
+typedef struct Constraint
+{
+    char event_name[EVENT_NAME_MAX + 1];
+    char* expr;
+    struct Constraint* next;
+} Constraint;
+
+
+static Constraint* new_Constraint(char** str, Read_state* state);
+
+
+static void del_Constraint(Constraint* constraint);
+
+
+typedef struct Target_event
+{
+    char* desc;
+    struct Target_event* next;
+} Target_event;
+
+
+static Target_event* new_Target_event(char** str, Read_state* state);
+
+
+static void del_Target_event(Target_event* event);
+
+
+typedef struct Cblist_item
+{
+    Constraint* constraints;
+    Target_event* events;
+    struct Cblist_item* next;
+} Cblist_item;
+
+
+static Cblist_item* new_Cblist_item(void);
+
+
+static void del_Cblist_item(Cblist_item* item);
+
+
 typedef struct Cblist
 {
     char event_name[EVENT_NAME_MAX + 1];
+    Cblist_item* first;
+    Cblist_item* last;
 } Cblist;
 
 
 static Cblist* new_Cblist(char* event_name);
 
 
+static void Cblist_append(Cblist* list, Cblist_item* item);
+
+
 static void del_Cblist(Cblist* list);
+
+
+static bool read_constraints(char** str,
+                             Read_state* state,
+                             Call_map* map,
+                             Cblist_item* item);
 
 
 Call_map* new_Call_map(char* str,
@@ -109,8 +161,19 @@ Call_map* new_Call_map(char* str,
                 return NULL;
             }
         }
+        Cblist_item* item = new_Cblist_item();
+        if (item == NULL)
+        {
+            del_Call_map(map);
+            return NULL;
+        }
+        Cblist_append(cblist, item);
         str = read_const_char(str, ',', state);
-        // TODO: read constraint list
+        if (!read_constraints(&str, state, map, item))
+        {
+            del_Call_map(map);
+            return NULL;
+        }
         str = read_const_char(str, ',', state);
         // TODO: read events to be fired
         str = read_const_char(str, ']', state);
@@ -122,6 +185,42 @@ Call_map* new_Call_map(char* str,
         check_next(str, state, expect_entry);
     }
     return map;
+}
+
+
+static bool read_constraints(char** str,
+                             Read_state* state,
+                             Call_map* map,
+                             Cblist_item* item)
+{
+    assert(str != NULL);
+    assert(*str != NULL);
+    assert(state != NULL);
+    assert(map != NULL);
+    assert(item != NULL);
+    *str = read_const_char(*str, '[', state);
+    if (state->error)
+    {
+        return false;
+    }
+    *str = read_const_char(*str, ']', state);
+    if (state->error)
+    {
+        Read_state_clear_error(state);
+        bool expect_entry = true;
+        while (expect_entry)
+        {
+            Constraint* constraint = new_Constraint(str, state);
+            if (constraint == NULL)
+            {
+                return false;
+            }
+            constraint->next = item->constraints;
+            item->constraints = constraint;
+            check_next(*str, state, expect_entry);
+        }
+    }
+    return !state->error;
 }
 
 
@@ -178,13 +277,149 @@ static Cblist* new_Cblist(char* event_name)
 }
 
 
+static void Cblist_append(Cblist* list, Cblist_item* item)
+{
+    assert(list != NULL);
+    assert(item != NULL);
+    if (list->last == NULL)
+    {
+        assert(list->first == NULL);
+        list->first = list->last = item;
+        return;
+    }
+    list->last->next = item;
+    list->last = item;
+    return;
+}
+
+
 static void del_Cblist(Cblist* list)
 {
     if (list == NULL)
     {
         return;
     }
+    Cblist_item* cur = list->first;
+    while (cur != NULL)
+    {
+        Cblist_item* next = cur->next;
+        del_Cblist_item(cur);
+        cur = next;
+    }
     xfree(list);
+    return;
+}
+
+
+static Cblist_item* new_Cblist_item(void)
+{
+    Cblist_item* item = xalloc(Cblist_item);
+    if (item == NULL)
+    {
+        return NULL;
+    }
+    item->constraints = NULL;
+    item->events = NULL;
+    item->next = NULL;
+    return item;
+}
+
+
+static void del_Cblist_item(Cblist_item* item)
+{
+    if (item == NULL)
+    {
+        return;
+    }
+    Constraint* curc = item->constraints;
+    while (curc != NULL)
+    {
+        Constraint* nextc = curc->next;
+        del_Constraint(curc);
+        curc = nextc;
+    }
+    Target_event* curt = item->events;
+    while (curt != NULL)
+    {
+        Target_event* nextt = curt->next;
+        del_Target_event(curt);
+        curt = nextt;
+    }
+    xfree(item);
+    return;
+}
+
+
+static Constraint* new_Constraint(char** str, Read_state* state)
+{
+    assert(str != NULL);
+    assert(*str != NULL);
+    assert(state != NULL);
+    if (state->error)
+    {
+        return NULL;
+    }
+    Constraint* c = xalloc(Constraint);
+    if (c == NULL)
+    {
+        return NULL;
+    }
+    c->expr = NULL;
+    c->next = NULL;
+    *str = read_const_char(*str, '[', state);
+    *str = read_string(*str, c->event_name, EVENT_NAME_MAX + 1, state);
+    *str = read_const_char(*str, ',', state);
+    char expr[1024] = "";
+    *str = read_string(*str, expr, 1024, state);
+    *str = read_const_char(*str, ']', state);
+    if (state->error)
+    {
+        del_Constraint(c);
+        return NULL;
+    }
+    c->expr = xcalloc(char, strlen(expr) + 1);
+    if (c->expr == NULL)
+    {
+        del_Constraint(c);
+        return NULL;
+    }
+    strcpy(c->expr, expr);
+    return c;
+}
+
+
+static void del_Constraint(Constraint* constraint)
+{
+    if (constraint == NULL)
+    {
+        return;
+    }
+    xfree(constraint->expr);
+    xfree(constraint);
+    return;
+}
+
+
+static Target_event* new_Target_event(char** str, Read_state* state)
+{
+    assert(str != NULL);
+    assert(*str != NULL);
+    assert(state != NULL);
+    if (state->error)
+    {
+        return NULL;
+    }
+}
+
+
+static void del_Target_event(Target_event* event)
+{
+    if (event == NULL)
+    {
+        return;
+    }
+    xfree(event->desc);
+    xfree(event);
     return;
 }
 
