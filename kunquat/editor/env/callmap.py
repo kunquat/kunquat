5468262814ddf33e4b7496c6f95c5ec244a2daec
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2012-02-01 17:06:01 +0200

    Added some more call map UI.

--- kunquat/editor/env/callmap.py
+++ kunquat/editor/env/callmap.py
@@ -15,6 +15,8 @@ from __future__ import division, print_function
 
 from PyQt4 import QtCore, QtGui
 
+import kunquat.editor.trigtypes as ttypes
+
 
 class CallMap(QtGui.QWidget):
 
@@ -28,6 +30,12 @@ class CallMap(QtGui.QWidget):
         layout.addWidget(self._fe, 0)
         self._bindspec = BindSpec()
         layout.addWidget(self._bindspec, 1)
+        QtCore.QObject.connect(self._fe,
+                               QtCore.SIGNAL('eventChanged(int)'),
+                               self._fe_changed)
+        QtCore.QObject.connect(self._fe,
+                               QtCore.SIGNAL('nameChanged(int, QString*)'),
+                               self._fe_name_changed)
         self.set_key('p_call_map.json')
 
     def set_key(self, key):
@@ -35,7 +43,7 @@ class CallMap(QtGui.QWidget):
         self.blockSignals(True)
         try:
             self._data = self._project[key] or []
-            self._fe.set_events(b[0] for b in self._data)
+            self._fe.events = (b[0] for b in self._data)
             self._fe_changed(max(0, min(len(self._data),
                                         self._fe.currentRow())))
         finally:
@@ -53,16 +61,63 @@ class CallMap(QtGui.QWidget):
             actions = []
         self._bindspec.set_spec(conditions, actions)
 
+    def _fe_name_changed(self, index, name):
+        name = str(name)
+        if index >= len(self._data):
+            assert index == len(self._data)
+            self._data.extend([[name, [], []]])
+        else:
+            self._data[index][0] = name
+        self._flatten()
+
+    def _flatten(self):
+        m = []
+        for binding in self._data:
+            if binding[0] not in ttypes.triggers or binding[0] == 'wj':
+                continue
+            c = []
+            for constraint in binding[1]:
+                if constraint[0] not in ttypes.triggers or \
+                        constraint[0] == 'wj' or not constraint[1]:
+                    continue
+                c.extend([[constraint[0], constraint[1]]])
+            a = []
+            for action in binding[2]:
+                if action[0] not in ttypes.triggers or action[0] == 'wj' or \
+                        not action[1][0]:
+                    continue
+                a.extend([[action[0], action[1]]])
+            m.extend([[binding[0], c, a]])
+        self._project[self._key] = m
+
 
 class FiringEvents(QtGui.QTableWidget):
+
+    eventChanged = QtCore.pyqtSignal(int, name='eventChanged')
+    nameChanged = QtCore.pyqtSignal(int, str, name='nameChanged')
 
     def __init__(self, parent=None):
         QtGui.QTableWidget.__init__(self, 0, 1, parent)
         self.setHorizontalHeaderLabels(['Event'])
         self.horizontalHeader().setResizeMode(0, QtGui.QHeaderView.Stretch)
         self.verticalHeader().hide()
+        QtCore.QObject.connect(self,
+                               QtCore.SIGNAL('currentCellChanged('
+                                             'int, int, int, int)'),
+                               self._cell_changed)
+        QtCore.QObject.connect(self,
+                               QtCore.SIGNAL('cellChanged(int, int)'),
+                               self._changed)
 
-    def set_events(self, events):
+    @property
+    def events(self):
+        events = []
+        for i in xrange(self.rowCount()):
+            events.extend([str(self.item(i, 0))])
+        return events
+
+    @events.setter
+    def events(self, events):
         self.blockSignals(True)
         try:
             index = 0
@@ -81,6 +136,20 @@ class FiringEvents(QtGui.QTableWidget):
 
     def _append_row(self):
         self.insertRow(self.rowCount())
+
+    def _cell_changed(self, row, col, prow, pcol):
+        if row != prow:
+            QtCore.QObject.emit(self,
+                                QtCore.SIGNAL('eventChanged(int)'),
+                                row)
+
+    def _changed(self, row, col):
+        text = self.item(row, col).text()
+        QtCore.QObject.emit(self,
+                            QtCore.SIGNAL('nameChanged(int, QString*)'),
+                            row, text)
+        if row == self.rowCount() - 1 and text:
+            self._append_row()
 
 
 class BindSpec(QtGui.QWidget):
