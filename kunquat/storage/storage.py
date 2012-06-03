537af39e1ef92735170220b47e0ac632e4fac856
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2012-06-03 19:44:18 +0300

    Increased version number and added missing legal headers.

--- kunquat/storage/storage.py
+++ kunquat/storage/storage.py
@@ -1,4 +1,16 @@
+# -*- coding: utf-8 -*-
 
+#
+# Author: Tomi Jylhä-Ollila, Finland 2012
+#         Toni Ruottu, Finland 2012
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
 
 import errno
 import os
