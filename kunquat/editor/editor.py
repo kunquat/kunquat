e2c068fb8cd182d991f5b2128d5d09d78acb3628
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2012-06-24 15:38:30 +0300

    Removed some placeholder widgets from the tracker.

--- kunquat/editor/editor.py
+++ kunquat/editor/editor.py
@@ -530,8 +530,8 @@ class KqtEditor(QtGui.QMainWindow):
         layout.addWidget(self.create_separator())
 
         layout.addWidget(self._pos_display)
-        layout.addWidget(subsong_select)
-        layout.addWidget(tempo_factor)
+        #layout.addWidget(subsong_select)
+        #layout.addWidget(tempo_factor)
         layout.addWidget(self.create_separator())
 
         layout.addWidget(self._instrument)
