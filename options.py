0cd48eeae8ae088623139e60ee34640a74f91226
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2012-06-24 15:23:07 +0300

    Disabled memory debugging tests by default and fixed version number.

--- options.py
+++ options.py
@@ -22,7 +22,7 @@ enable_python_bindings = True
 enable_tests = True
 
 # Run tests with memory debugging (requires valgrind, disables assert tests).
-enable_tests_mem_debug = True
+enable_tests_mem_debug = False
 
 # Enable kunquat-player (requires enable_python_bindings).
 enable_player = True
