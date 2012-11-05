4f42257635c980f0be3a62de96b7175eb629dbc9
Toni Ruottu <toni.ruottu@iki.fi>
2012-11-05 23:21:28 +0200

    old orderlist hack separated

--- kunquat/tracker/song.py
+++ kunquat/tracker/song.py
@@ -10,21 +10,23 @@ class Song():
         name = self._view.get_json('m_name.json') or '-'
         return name
 
-    def _count_each(self, l):
-        counters = {}
-        for item in l:
-            try:
-                value = counters[item]
-            except KeyError:
-                value = 0
-            yield [item, value]
-            counters[item] = value + 1
-        
-    def get_order_list(self):
+    def _old_orderlist_compatibility_hack(self):
+        def _count_each(l):
+            counters = {}
+            for item in l:
+                try:
+                    value = counters[item]
+                except KeyError:
+                    value = 0
+                yield [item, value]
+                counters[item] = value + 1
         if 'p_orderlist.json' not in self._view.keys():
             songdata = self._view.get_json('p_song.json')
             old_list = songdata['patterns']
-            instances = list(self._count_each(old_list))
+            instances = list(_count_each(old_list))
             self._view.put('p_orderlist.json', instances)
+        
+    def get_order_list(self):
+        self._old_orderlist_compatibility_hack()
         orderlist = self._view.get_json('p_orderlist.json')
         return orderlist
