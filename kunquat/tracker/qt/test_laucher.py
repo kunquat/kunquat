e6cd245ec0e0d1a97b580c2f8fb78a6831322569
Toni Ruottu <toni.ruottu@iki.fi>
2013-03-24 23:04:38 +0200

    test ui launching

--- kunquat/tracker/qt/test_laucher.py
+++ kunquat/tracker/qt/test_laucher.py
@@ -16,16 +16,26 @@ import unittest
 from launcher import QtLauncher
 
 
-class Dummy():
-    pass
+class DummyFrontend():
+
+    def __init__(self):
+        pass
 
 
 class TestLauncher(unittest.TestCase):
 
-    def test_setters(self):
-        ui_launcher = QtLauncher()
-        ui_launcher.set_frontend(Dummy())
-        ui_launcher.set_queue_processor(Dummy())
+    def _dummyQueueProcessor(self):
+        if self._call_counter > 0:
+            self._call_counter -= 1
+            return  
+        self._ui_launcher.halt_ui()
+
+    def test_launcher(self):
+        self._call_counter = 2
+        self._ui_launcher = QtLauncher(show = False)
+        self._ui_launcher.set_frontend(DummyFrontend())
+        self._ui_launcher.set_queue_processor(self._dummyQueueProcessor)
+        self._ui_launcher.run_ui()
 
 
 if __name__ == '__main__':
