c551a67f59915c3df2d3f8748e8d75b108098b11
Toni Ruottu <toni.ruottu@iki.fi>
2013-03-24 12:35:31 +0200

    separate general tools from pulseaudio driver class

--- kunquat/tracker/audio/drivers/pulseaudio.py
+++ kunquat/tracker/audio/drivers/pulseaudio.py
@@ -19,6 +19,31 @@ import Queue
 
 from kunquat.extras.pulseaudio_async import Async
 
+def split_list_at(lst, i):
+    """
+    >>> split_list_at([1,2,3], 1)
+    ([1], [2, 3])
+    """
+    first = lst[:i]
+    second = lst[i:]
+    return (first, second)
+
+def split_audio_at(audio, i):
+    (left, right) = audio
+    (left1, left2) = split_list_at(left, i)
+    (right1, right2) = split_list_at(right, i)
+    audio1 = (left1, right1)
+    audio2 = (left2, right2)
+    return (audio1, audio2)
+
+def join_audio(audio1, audio2):
+    (left1, right1) = audio1
+    (left2, right2) = audio2
+    left = left1 + left2
+    right = right1 + right2
+    audio = (left, right)
+    return audio
+
 class Pulseaudio():
 
     def __init__(self):
@@ -40,39 +65,18 @@ class Pulseaudio():
     def _next(self, nframes):
         self._ag.generate(nframes)
 
-    def _split_list_at(self, lst, i):
-        first = lst[:i]
-        second = lst[i:]
-        return (first, second)
-
-    def _split_audio_at(self, audio, i):
-        (left, right) = audio
-        (left1, left2) = self._split_list_at(left, i)
-        (right1, right2) = self._split_list_at(right, i)
-        audio1 = (left1, right1)
-        audio2 = (left2, right2)
-        return (audio1, audio2)
-
-    def _join_audio(self, audio1, audio2):
-        (left1, right1) = audio1
-        (left2, right2) = audio2
-        left = left1 + left2
-        right = right1 + right2
-        audio = (left, right)
-        return audio
-
     def _update_workspace(self):
         items = self._buffer.qsize()
         for _ in range(items):
             fresh_audio = self._buffer.get()
-            self._workspace = self._join_audio(self._workspace, fresh_audio)
+            self._workspace = join_audio(self._workspace, fresh_audio)
 
     def _read_workspace(self, nframes):
         (left, right) = self._workspace
         assert len(left) == len(right)
         frames = len(left)
         if frames > nframes:
-            (audio_data, remainder) = self._split_audio_at(self._workspace, nframes)
+            (audio_data, remainder) = split_audio_at(self._workspace, nframes)
         else:
             audio_data = self._workspace
             remainder = ([],[])
@@ -91,4 +95,8 @@ class Pulseaudio():
 
     def stop(self):
         self._pa.stop()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
