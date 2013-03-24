cea28f8539918839691243290ad6213a6ab707ed
Toni Ruottu <toni.ruottu@iki.fi>
2013-03-25 00:46:23 +0200

    accept int as argument in command queue

--- kunquat/tracker/threads/commandqueue.py
+++ kunquat/tracker/threads/commandqueue.py
@@ -22,7 +22,7 @@ class CommandQueue(Queue):
 
     def put(self, command):
         arg_type = type(command.arg)
-        if not arg_type in [type(''), type(None), Thread]:
+        if not arg_type in [type(''), type(None), type(2), Thread]:
             raise TypeError
         Queue.put(self, command)
 
