d717bc529fe710ff47e357b1f224c4d93313225c
Toni Ruottu <toni.ruottu@iki.fi>
2013-03-29 01:41:15 +0200

    consider tuples immutable when they contain immutable values

--- kunquat/tracker/threads/commandqueue.py
+++ kunquat/tracker/threads/commandqueue.py
@@ -15,14 +15,42 @@ from Queue import Queue
 from threading import Thread
 
 
+def is_immutable(value):
+    """
+    >>> is_immutable('hello')
+    True
+    >>> is_immutable(None)
+    True
+    >>> is_immutable(14)
+    True
+    >>> is_immutable(long(20))
+    True
+    >>> is_immutable([1, 2, 3])
+    False
+    >>> is_immutable((1, 2))
+    True
+    >>> is_immutable((1, [2, 3]))
+    False
+    """
+    if type(value) == tuple:
+        for v in value:
+            if not is_immutable(v):
+                return False
+        return True
+    immutable_types = [str, int, long, type(None)]
+    result = (type(value) in immutable_types)
+    return result
+
+
 class CommandQueue(Queue):
 
     def __init__(self):
         Queue.__init__(self)
 
     def put(self, command):
-        arg_type = type(command.arg)
-        if not arg_type in [type(''), type(None), type(2), type(long(2)), Thread]:
+        arg = command.arg
+        if is_immutable(arg) or type(arg) == Thread:
+            Queue.put(self, command)
+        else:
             raise TypeError
-        Queue.put(self, command)
 
