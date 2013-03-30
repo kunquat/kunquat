2995cba81970a8bc2a741f4a6e5c9bc8d7747819
Toni Ruottu <toni.ruottu@iki.fi>
2013-03-30 22:46:19 +0200

    separate shared thread tests

---
+++ kunquat/tracker/threads/test_abstractthread.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+#
+# Author: Toni Ruottu, Finland 2013
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
+
+import inspect
+import unittest
+import threading
+
+
+class Recorder(object):
+
+    def __init__(self, put_record):
+        self._put_record = put_record
+
+    def __call__(self, name, *args, **kwargs):
+        record = (name, args, kwargs)
+        self._put_record(record)
+
+
+def public_interface(some_class):
+    members = inspect.getmembers(some_class)
+    interface = [name for (name, _) in members if not name.startswith('_')]
+    return interface
+
+
+class TestAbstractThread():
+    """
+    Abstract thread tester class. The class inheriting this class also
+    needs to inherit unittest.TestCase The abstract class can not inherit
+    TestCase itself since it does not implement a standalone testcase.
+    """
+
+    def setUp(self):
+        raise NotImplementedError
+
+    def test_halt(self):
+        thread = self._TestClass(None)
+        thread.halt()
+        thread.run()
+
+    def test_interface(self):
+        interface_spec = set(public_interface(self._InterfaceClass))
+        implementation = set(public_interface(self._TestClass))
+        missing_members = interface_spec - implementation
+        self.assertEqual(missing_members, set())
+
+    def _put_record(self, record):
+        (name, _, _) = record
+        self._records[name] = record
+
+    def test_argument_passing(self):
+        self._records = {}
+        recorder = Recorder(self._put_record)
+        thread = self._TestClass(recorder)
+        for call in self._test_calls:
+            (method, args, kwargs) = call
+            getattr(thread, method)(*args, **kwargs)
+        self.assertTrue(len(self._records) < 1)
+        thread.halt()
+        thread.run()
+        for call in self._test_calls:
+            (name, _, _) = call
+            record = self._records[name]
+            self.assertEqual(call, record)
+        called_methods = set(self._records.keys())
+        interface_spec = set(public_interface(self._InterfaceClass))
+        not_tested = interface_spec - called_methods
+        self.assertEqual(not_tested, set())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
--- kunquat/tracker/threads/test_backendthread.py
+++ kunquat/tracker/threads/test_backendthread.py
@@ -11,39 +11,19 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
-import inspect
 import unittest
 import threading
 
 from kunquat.tracker.backend.backend import Backend
 from backendthread import BackendThread
-
-
-class Recorder(object):
-
-    def __init__(self, put_record):
-        self._put_record = put_record
-
-    def __call__(self, name, *args, **kwargs):
-        record = (name, args, kwargs)
-        self._put_record(record)
+from test_abstractthread import TestAbstractThread
 
 
 class DummyFrontend(threading.Thread):
     pass
 
 
-class DummyBackend():
-    pass
-
-
-def public_interface(some_class):
-    members = inspect.getmembers(some_class)
-    interface = [name for (name, _) in members if not name.startswith('_')]
-    return interface
-
-
-class TestBackendthread(unittest.TestCase):
+class TestBackendthread(TestAbstractThread, unittest.TestCase):
 
     def setUp(self):
         self._test_calls = [
@@ -55,40 +35,6 @@ class TestBackendthread(unittest.TestCase):
         ]
         self._InterfaceClass = Backend
         self._TestClass = BackendThread
-
-    def test_halt(self):
-        thread = self._TestClass(None)
-        thread.halt()
-        thread.run()
-
-    def test_interface(self):
-        interface_spec = set(public_interface(self._InterfaceClass))
-        implementation = set(public_interface(self._TestClass))
-        missing_members = interface_spec - implementation
-        self.assertEqual(missing_members, set())
-
-    def _put_record(self, record):
-        (name, _, _) = record
-        self._records[name] = record
-
-    def test_argument_passing(self):
-        self._records = {}
-        recorder = Recorder(self._put_record)
-        thread = self._TestClass(recorder)
-        for call in self._test_calls:
-            (method, args, kwargs) = call
-            getattr(thread, method)(*args, **kwargs)
-        self.assertTrue(len(self._records) < 1)
-        thread.halt()
-        thread.run()
-        for call in self._test_calls:
-            (name, _, _) = call
-            record = self._records[name]
-            self.assertEqual(call, record)
-        called_methods = set(self._records.keys())
-        interface_spec = set(public_interface(self._InterfaceClass))
-        not_tested = interface_spec - called_methods
-        self.assertEqual(not_tested, set())
 
 
 if __name__ == '__main__':
