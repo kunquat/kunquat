233ed938c24533a9d4bf1b9cf27ac5e0872fc769
Toni Ruottu <toni.ruottu@iki.fi>
2013-04-01 12:45:24 +0300

    set up test state for frontend tests

---
+++ kunquat/tracker/frontend/test_frontend.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#
+# Author: Toni Ruottu, Finland 2013
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
+
+import unittest
+
+from frontend import Frontend
+
+
+class TestBackend(unittest.TestCase):
+
+    def setUp(self):
+        self._frontend = Frontend()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
