81be7055915ee4e9959af9bebef4425e561604b6
Toni Ruottu <toni.ruottu@iki.fi>
2013-04-07 01:26:05 +0300

    separate pushaudio driver and audio pump

--- kunquat/tracker/audio/drivers/pushaudio.py
+++ kunquat/tracker/audio/drivers/pushaudio.py
@@ -12,51 +12,16 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
-from Queue import Queue
-import threading
-
+from kunquat.tracker.audio.audio_pump import AudioPump
 from kunquat.extras.pulseaudio import Simple
-
-HALT = None
-
-class AudioPump(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self._audio_source = None
-        self._buffer = Queue()
-        self._pa = Simple('Kunquat Tracker',
-                          'Editor output')
-
-    def set_audio_source(self, audio_source):
-        self._audio_source = audio_source
-
-    def put_audio(self, audio_data):
-        self._buffer.put(audio_data)
-
-    def _pump(self):
-        nframes = 2048
-        self._audio_source.generate_audio(nframes)
-        audio_data = self._buffer.get()
-        (left, right) = audio_data
-        if len(left) > 0:
-            self._pa.write(left, right)
-
-    def halt(self):
-        self._buffer.put(HALT)
-
-    def run(self):
-        audio_data = self._buffer.get()
-        while audio_data != HALT:
-            self._pump()
-            audio_data = self._buffer.get()
-        del self._pa
-
 
 class Pushaudio():
 
     def __init__(self):
         self._audio_pump = AudioPump()
+        self._audio_pump.set_write_method(self._put_audio)
+        self._pa = Simple('Kunquat Tracker',
+                          'Editor output')
 
     def __del__(self):
         if self._audio_pump.is_alive():
@@ -64,6 +29,11 @@ class Pushaudio():
 
     def set_audio_source(self, audio_source):
         self._audio_pump.set_audio_source(audio_source)
+
+    def _put_audio(self, audio_data):
+        (left, right) = audio_data
+        if len(left) > 0:
+            self._pa.write(left, right)
 
     def put_audio(self, audio_data):
         self._audio_pump.put_audio(audio_data)
@@ -77,6 +47,7 @@ class Pushaudio():
 
     def close(self):
         self.stop()
+        del self._pa
 
     @classmethod
     def get_id(cls):
