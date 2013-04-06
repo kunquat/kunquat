abfd34b7653bf68095f28668f9b4cdd5690cbcda
Toni Ruottu <toni.ruottu@iki.fi>
2013-04-07 01:45:42 +0300

    test audio pump halt

---
+++ kunquat/tracker/audio/test_audio_pump.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+#
+# Author: Toni Ruottu, Finland 2013
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
+
+import unittest
+from audio_pump import AudioPump
+
+class TestAbstractThread(unittest.TestCase):
+
+    def setUp(self):
+        self._thread = AudioPump()
+
+    def test_halt(self):
+        self._thread.halt()
+        self._thread.run()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
