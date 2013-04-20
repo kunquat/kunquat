fe82317930f738f691bc5ce28bb13d7883ed49f5
Toni Ruottu <toni.ruottu@iki.fi>
2013-04-20 09:27:24 +0300

    prototype module importing

--- kunquat/tracker/backend/backend.py
+++ kunquat/tracker/backend/backend.py
@@ -12,7 +12,9 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
+import sys
 from itertools import islice
+import tarfile
 import math
 from kunquat import Kunquat
 
@@ -25,12 +27,42 @@ def gen_sine(rate):
         yield math.sin(phase) * 0.3
 
 
+def remove_prefix(path, prefix):
+    preparts = prefix.split('/')
+    keyparts = path.split('/')
+    for pp in preparts:
+        kp = keyparts.pop(0)
+        if pp != kp:
+             return None
+    return '/'.join(keyparts)
+
+
 class Backend():
 
     def __init__(self):
         self._audio_output = None
         self._frontend = None
         self._kunquat = Kunquat()
+
+        path = sys.argv[1]
+        prefix = 'kqtc00'
+        tfile = tarfile.open(path, format=tarfile.USTAR_FORMAT)
+        for entry in tfile.getmembers():
+            tarpath = entry.name
+            key = remove_prefix(tarpath, prefix)
+            assert (key != None) #TODO broken file exception
+            if entry.isfile():
+                value = tfile.extractfile(entry).read()
+                print 100 * '-'
+                print key
+                if key.endswith('.wv'):
+                    print '<%sB wav>' % len(value)
+                else:
+                    print value
+                print 100 * '-'
+                self._kunquat.set_data(key, value)
+        tfile.close()
+        self._kunquat.validate()
 
         self._sine = gen_sine(48000)
 
