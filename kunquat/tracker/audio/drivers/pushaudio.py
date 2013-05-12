a6dc4accb47843da8f7ef543726a5e2883503f31
Toni Ruottu <toni.ruottu@iki.fi>
2013-05-13 00:05:11 +0300

    implement push based interface in push audio driver

--- kunquat/tracker/audio/drivers/pushaudio.py
+++ kunquat/tracker/audio/drivers/pushaudio.py
@@ -12,39 +12,29 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
-from kunquat.tracker.audio.audio_pump import AudioPump
 from kunquat.extras.pulseaudio import Simple
 
 class Pushaudio():
 
     def __init__(self):
-        self._audio_pump = AudioPump()
-        self._audio_pump.set_write_method(self._put_audio)
+        self._audio_source = None
         self._pa = Simple('Kunquat Tracker',
                           'Editor output')
 
-    def __del__(self):
-        if self._audio_pump.is_alive():
-            self_audio_pump.halt()
-            self_audio_pump.join()
-
     def set_audio_source(self, audio_source):
-        self._audio_pump.set_audio_source(audio_source)
+        self._audio_source = audio_source
 
-    def _put_audio(self, audio_data):
+    def put_audio(self, audio_data):
         (left, right) = audio_data
         if len(left) > 0:
             self._pa.write(left, right)
-
-    def put_audio(self, audio_data):
-        self._audio_pump.put_audio(audio_data)
+        self._audio_source.acknowledge_audio()
 
     def start(self):
-        self._audio_pump.start()
+        pass
 
     def stop(self):
-        self._audio_pump.halt()
-        self._audio_pump.join()
+        pass
 
     def close(self):
         self.stop()
