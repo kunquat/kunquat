35401fe67fe7ddab1fc44e16df99071d28dd3682
Toni Ruottu <toni.ruottu@iki.fi>
2013-05-18 22:43:00 +0300

    use cpickle for commands

--- kunquat/tracker/threads/command.py
+++ kunquat/tracker/threads/command.py
@@ -12,7 +12,7 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
-import pickle
+import cPickle
 
 from threading import Thread
 
@@ -55,14 +55,14 @@ class Command():
     def _freeze_arg(self, arg):
         if isinstance(arg, Thread):
             return (ARG_THREAD, arg)
-        return (ARG_PICKLE, pickle.dumps(arg))
+        return (ARG_PICKLE, cPickle.dumps(arg))
 
     def _melt_arg(self, frozen):
         (arg_type, value) = frozen
         if arg_type == ARG_THREAD:
             return value
         if arg_type == ARG_PICKLE:
-            return pickle.loads(value)
+            return cPickle.loads(value)
         assert(False)
 
 
