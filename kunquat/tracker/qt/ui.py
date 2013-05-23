7098227588b779cd3cb8024149986a8d30276a83
Toni Ruottu <toni.ruottu@iki.fi>
2013-05-24 00:44:07 +0300

    select pushaudio driver at startup

--- kunquat/tracker/qt/ui.py
+++ kunquat/tracker/qt/ui.py
@@ -42,14 +42,15 @@ class Ui():
         self._mainwindow = MainWindow()
         self._qp = None
         self._event_pump = EventPump()
-        self._driver_switch_timer = QTimer()
         self._update_timer = QTimer()
         self._ui_model = None
-        self._asdfasdf = False
 
     def set_ui_model(self, ui_model):
         self._ui_model = ui_model
         self._mainwindow.set_ui_model(ui_model)
+        from kunquat.tracker.audio.drivers.pushaudio import Pushaudio
+        driver_manager = self._ui_model.get_driver_manager()
+        driver_manager.set_selected_driver(Pushaudio)
 
     def set_queue_processor(self, qp, block):
         assert not self._qp
@@ -60,31 +61,6 @@ class Ui():
                 SIGNAL('process_queue()'),
                 self._qp)
         self._event_pump.start()
-
-    def select_random_driver(self):
-        if self._asdfasdf:
-            return
-        self._asdfasdf = True
-        from kunquat.tracker.audio.drivers.pushaudio import Pushaudio
-        from kunquat.tracker.audio.drivers.nullaudio import Nullaudio
-        from kunquat.tracker.audio.drivers.silentaudio import Silentaudio
-        one = Pushaudio
-        print 'driver: %s' % one.get_id()
-        driver_manager = self._ui_model.get_driver_manager()
-        driver_manager.set_selected_driver(one)
-        self._ui_model.load_module()
-
-        #import random
-        #driver_manager = self._ui_model.get_driver_manager()
-        #drivers = driver_manager.get_drivers()
-        #one = random.choice(drivers)
-
-    def _start_driver_randomizer(self):
-        QObject.connect(
-                self._driver_switch_timer,
-                SIGNAL('timeout()'),
-                self.select_random_driver)
-        self._driver_switch_timer.start(3000)
 
     def _start_updater(self):
         QObject.connect(
@@ -102,7 +78,7 @@ class Ui():
 
     def run(self):
         self._start_updater()
-        self._start_driver_randomizer()
+        self._ui_model.load_module()
         self._app.exec_()
 
 
