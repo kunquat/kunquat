28e5b44118155000410ecf3fe3eb40f0d934d3c2
Toni Ruottu <toni.ruottu@iki.fi>
2013-05-26 11:39:53 +0300

    implement led in type writer button

--- kunquat/tracker/qt/type_writer_button.py
+++ kunquat/tracker/qt/type_writer_button.py
@@ -14,14 +14,81 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-class TypeWriterButton(QToolButton):
+class TWLed(QFrame):
+
+    def __init__(self, color_style):
+        super(QFrame, self).__init__()
+        self.setMinimumWidth(35)
+        self.setMinimumHeight(15)
+        self.setMaximumWidth(35)
+        self.setMaximumHeight(15)
+        self.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        self.setLineWidth(2)
+        self._color_style = color_style
+        self._light = 8
+        self._enabled = True
+
+        self._left = QLabel()
+        self._left.setMargin(0)
+        self._left.setMaximumWidth(5)
+        self._center = QLabel()
+        self._center.setMargin(0)
+        self._right = QLabel()
+        self._right.setMargin(0)
+        self._right.setMaximumWidth(5)
+
+        h = QHBoxLayout()
+        h.addWidget(self._left)
+        h.addWidget(self._center)
+        h.addWidget(self._right)
+        h.setContentsMargins(0,0,0,0)
+        h.setSpacing(0)
+        self.setLayout(h)
+        self._silent()
+
+    def _flat(self):
+        self._left.setStyleSheet("QLabel { background-color: #f00; }")
+        self._center.setStyleSheet("QLabel { background-color: #500; }")
+        self._right.setStyleSheet("QLabel { background-color: #400; }")
+
+    def _exact(self):
+        self._left.setStyleSheet("QLabel { background-color: #f00; }")
+        self._center.setStyleSheet("QLabel { background-color: #f00; }")
+        self._right.setStyleSheet("QLabel { background-color: #f00; }")
+
+    def _sharp(self):
+        self._left.setStyleSheet("QLabel { background-color: #400; }")
+        self._center.setStyleSheet("QLabel { background-color: #500; }")
+        self._right.setStyleSheet("QLabel { background-color: #f00; }")
+
+    def _silent(self):
+        self._left.setStyleSheet("QLabel { background-color: #400; }")
+        self._center.setStyleSheet("QLabel { background-color: #400; }")
+        self._right.setStyleSheet("QLabel { background-color: #400; }")
+
+class TypeWriterButton(QPushButton):
 
     def __init__(self):
-        QToolButton.__init__(self)
+        QPushButton.__init__(self)
         self._instrument = None
 
-        self.setText('100c')
-        self.setAutoRaise(True)
+        self.setMinimumWidth(60)
+        self.setMinimumHeight(60)
+        layout = QVBoxLayout(self)
+        led = TWLed(100)
+        self._led = led
+        layout.addWidget(led)
+        notename = QLabel()
+        self._notename = notename
+        notename.setAlignment(Qt.AlignCenter)
+        layout.addWidget(notename)
+        layout.setAlignment(Qt.AlignCenter)
+        self.setFocusPolicy(Qt.NoFocus)
+
+        self._notename.setText('100c')
+        self.setStyleSheet("QLabel { background-color: #ffe; }")
+        self._notename.setStyleSheet("QLabel { color: #000; }")
+
 
     def _play_sound(self):
         self._instrument.set_active_note(0, 100)
