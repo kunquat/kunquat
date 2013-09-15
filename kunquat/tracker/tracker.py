7b16773e5812dbf07e87c306547093d9fb857491
Toni Ruottu <toni.ruottu@iki.fi>
2013-09-15 15:29:24 +0300

    create new hello tracker

---
+++ kunquat/tracker/tracker.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+#
+# Author: Toni Ruottu, Finland 2013
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
+
+def main():
+
+    print 'hello'
