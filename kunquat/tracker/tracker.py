632f6aeb7fb8397364eda3983187fb8c685903f4
Toni Ruottu <toni.ruottu@iki.fi>
2013-09-15 18:21:11 +0300

    connect ui to the ui model

--- kunquat/tracker/tracker.py
+++ kunquat/tracker/tracker.py
@@ -18,9 +18,34 @@ import sys
 
 from widgets.mainwindow import MainWindow
 
+from kunquat.tracker.model.driver_manager import DriverManager
+from kunquat.tracker.model.stat_manager import StatManager
+from kunquat.tracker.model.ui_manager import UiManager
+from kunquat.tracker.model.playback_manager import PlaybackManager
+from kunquat.tracker.model.module import Module
+from kunquat.tracker.model.uimodel import UiModel
+
+def create_ui_model():
+    #drivers = [Nullaudio, Pulseaudio, Pushaudio, Silentaudio]
+    driver_manager = DriverManager()
+    #driver_manager.set_drivers(drivers)
+    stat_manager = StatManager()
+    ui_manager = UiManager()
+    playback_manager = PlaybackManager()
+    module = Module()
+    ui_model = UiModel()
+    ui_model.set_driver_manager(driver_manager)
+    ui_model.set_stat_manager(stat_manager)
+    ui_model.set_ui_manager(ui_manager)
+    ui_model.set_playback_manager(playback_manager)
+    ui_model.set_module(module)
+    return ui_model
+
 def main():
     app = QApplication(sys.argv)
     main_window = MainWindow()
+    ui_model = create_ui_model()
+    main_window.set_ui_model(ui_model)
     main_window.show()
     app.exec_()
 
