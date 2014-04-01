ee75bdae5b2a53267664652744a392dfcd8ace20
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-04-01 23:35:11 +0300

    Combine two separate jump event code modules into one.

--- src/lib/player/events/Event_master_jump.c
+++ src/lib/player/events/Event_master_jump.c
@@ -20,7 +20,50 @@
 #include <kunquat/limits.h>
 #include <player/events/Event_common.h>
 #include <player/events/Event_master_decl.h>
+#include <Value.h>
 #include <xassert.h>
+
+
+bool Event_master_set_jump_counter_process(
+        Master_params* master_params,
+        Value* value)
+{
+    assert(master_params != NULL);
+    assert(value != NULL);
+    assert(value->type == VALUE_TYPE_INT);
+
+    master_params->jump_counter = value->value.int_type;
+
+    return true;
+}
+
+
+bool Event_master_set_jump_row_process(
+        Master_params* master_params,
+        Value* value)
+{
+    assert(master_params != NULL);
+    assert(value != NULL);
+    assert(value->type == VALUE_TYPE_TSTAMP);
+
+    Tstamp_copy(&master_params->jump_target_row, &value->value.Tstamp_type);
+
+    return true;
+}
+
+
+bool Event_master_set_jump_pat_inst_process(
+        Master_params* master_params,
+        Value* value)
+{
+    assert(master_params != NULL);
+    assert(value != NULL);
+    assert(value->type == VALUE_TYPE_PAT_INST_REF);
+
+    master_params->jump_target_piref = value->value.Pat_inst_ref_type;
+
+    return true;
+}
 
 
 bool Event_master_jump_process(Master_params* master_params, Value* value)
