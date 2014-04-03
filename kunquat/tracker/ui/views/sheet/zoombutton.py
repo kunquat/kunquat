738c2174d99105916a86972270482a281d007a32
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-04-03 22:43:34 +0300

    Change key combination style.

--- kunquat/tracker/ui/views/sheet/zoombutton.py
+++ kunquat/tracker/ui/views/sheet/zoombutton.py
@@ -18,12 +18,12 @@ from PyQt4.QtGui import *
 class ZoomButton(QToolButton):
 
     INFO = {
-            'in': ('Zoom In', 'zoom-in', 'Ctrl +'),
-            'out': ('Zoom Out', 'zoom-out', 'Ctrl -'),
-            'original': ('Zoom to Original', 'zoom-original', 'Ctrl+0'),
-            'expand_w': ('Wider', None, 'Ctrl+Alt +'),
-            'shrink_w': ('Narrower', None, 'Ctrl+Alt -'),
-            'original_w': ('Original Width', None, 'Ctrl+Alt+0'),
+            'in': ('Zoom In', 'zoom-in', 'Ctrl + +'),
+            'out': ('Zoom Out', 'zoom-out', 'Ctrl + -'),
+            'original': ('Zoom to Original', 'zoom-original', 'Ctrl + 0'),
+            'expand_w': ('Wider', None, 'Ctrl + Alt + +'),
+            'shrink_w': ('Narrower', None, 'Ctrl + Alt + -'),
+            'original_w': ('Original Width', None, 'Ctrl + Alt + 0'),
         }
 
     def __init__(self, mode):
