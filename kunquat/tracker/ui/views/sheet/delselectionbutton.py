50675dfc3ac37055172c0ffbb0ae071de233cc1c
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-04-12 15:05:49 +0300

    Use sheet manager for retrieving the current column in delete selection button.

--- kunquat/tracker/ui/views/sheet/delselectionbutton.py
+++ kunquat/tracker/ui/views/sheet/delselectionbutton.py
@@ -21,6 +21,7 @@ class DelSelectionButton(QToolButton):
         QToolButton.__init__(self)
         self._ui_model = None
         self._updater = None
+        self._sheet_manager = None
 
         self.setText('Del')
         self.setToolTip('Delete selection (Delete)')
@@ -29,6 +30,8 @@ class DelSelectionButton(QToolButton):
         self._ui_model = ui_model
         self._updater = ui_model.get_updater()
         self._updater.register_updater(self._perform_updates)
+        self._sheet_manager = ui_model.get_sheet_manager()
+
         self._update_enabled()
 
     def unregister_updaters(self):
@@ -41,19 +44,10 @@ class DelSelectionButton(QToolButton):
     def _update_enabled(self):
         selection = self._ui_model.get_selection()
         location = selection.get_location()
+        cur_column = self._sheet_manager.get_column_at_location(location)
 
-        has_trigger = False
-
-        module = self._ui_model.get_module()
-        album = module.get_album()
-
-        if album and album.get_track_count() > 0:
-            cur_song = album.get_song_by_track(location.get_track())
-            cur_pattern = cur_song.get_pattern_instance(
-                    location.get_system()).get_pattern()
-            cur_column = cur_pattern.get_column(location.get_col_num())
-            has_trigger = cur_column.has_trigger(
-                    location.get_row_ts(), location.get_trigger_index())
+        has_trigger = bool(cur_column) and cur_column.has_trigger(
+                location.get_row_ts(), location.get_trigger_index())
 
         self.setEnabled(has_trigger)
 
