2adb1ea28ab07d1d4c8acd6f598b3e8bd8980223
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-04-13 11:56:03 +0300

    Create sandbox if no module is loaded.

--- kunquat/tracker/ui/controller/controller.py
+++ kunquat/tracker/ui/controller/controller.py
@@ -85,6 +85,17 @@ class Controller():
                  return None
         return '/'.join(keyparts)
 
+    def create_sandbox(self):
+        transaction = {
+            'album/p_manifest.json' : {},
+            'album/p_tracks.json'   : [0],
+            'song_00/p_manifest.json': {},
+            'song_00/p_order_list.json': [[0, 0]],
+            'pat_000/p_manifest.json': {},
+            'pat_000/instance_000/p_manifest.json': {},
+        }
+        self._store.put(transaction)
+
     def get_task_load_module(self, module_path):
         values = dict()
         if module_path[-4:] in ['.kqt', '.bz2']:
