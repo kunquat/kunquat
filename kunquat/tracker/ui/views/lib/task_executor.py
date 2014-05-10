17457797227369142547fece91eea481db2888e7
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-05-10 14:50:34 +0300

    Implement default task executor.

---
+++ kunquat/tracker/ui/views/lib/task_executor.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#
+# Author: Tomi Jylhä-Ollila, Finland 2014
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+
+def execute_task(task):
+    for _ in task:
+        QApplication.processEvents()
+
+
