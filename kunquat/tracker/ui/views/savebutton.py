9e5c99c0efd9b2db9514798af88d8c9af3791cf3
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-06-02 22:10:22 +0300

    Keep save button disabled until loading has finished.

--- kunquat/tracker/ui/views/savebutton.py
+++ kunquat/tracker/ui/views/savebutton.py
@@ -20,15 +20,23 @@ class SaveButton(QToolButton):
     def __init__(self):
         QToolButton.__init__(self)
         self._ui_model = None
+        self._updater = None
 
         self.setText('Save')
+        self.setEnabled(False)
 
     def set_ui_model(self, ui_model):
         self._ui_model = ui_model
+        self._updater = ui_model.get_updater()
+        self._updater.register_updater(self._perform_updates)
         QObject.connect(self, SIGNAL('clicked()'), self._clicked)
 
     def unregister_updaters(self):
-        pass
+        self._updater.unregister_updater(self._perform_updates)
+
+    def _perform_updates(self, signals):
+        if 'signal_module' in signals:
+            self.setEnabled(True)
 
     def _clicked(self):
         module = self._ui_model.get_module()
