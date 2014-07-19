6f6bf32b9d6c7285595e669ea3d263b07c511f52
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-07-19 19:54:57 +0300

    Implement tremolo speed slide.

--- src/lib/player/events/Event_channel_types.h
+++ src/lib/player/events/Event_channel_types.h
@@ -34,6 +34,7 @@ EVENT_CHANNEL_DEF("/f",     slide_force,            FLOAT,          v_force)
 EVENT_CHANNEL_DEF("/=f",    slide_force_length,     TSTAMP,         v_nonneg_ts)
 EVENT_CHANNEL_DEF("ts",     tremolo_speed,          FLOAT,          v_nonneg_float)
 EVENT_CHANNEL_DEF("td",     tremolo_depth,          FLOAT,          v_tremolo_depth)
+EVENT_CHANNEL_DEF("t/=s",   tremolo_speed_slide,    TSTAMP,         v_nonneg_ts)
 EVENT_CHANNEL_DEF("t/=d",   tremolo_depth_slide,    TSTAMP,         v_nonneg_ts)
 
 EVENT_CHANNEL_DEF("/p",     slide_pitch,            FLOAT,          v_pitch)
