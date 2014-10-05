6cb1b37b03e4d89635e75ba0779ce2f386c09cb9
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-10-05 14:58:49 +0300

    Add global force slider to instrument editor.

--- kunquat/tracker/ui/views/instrument/editor.py
+++ kunquat/tracker/ui/views/instrument/editor.py
@@ -15,6 +15,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from name import Name
+from globalforce import GlobalForce
 
 
 class Editor(QWidget):
@@ -24,20 +25,25 @@ class Editor(QWidget):
         self._ui_model = None
         self._ins_id = None
         self._name = Name()
+        self._global_force = GlobalForce()
 
         v = QVBoxLayout()
         v.addWidget(self._name)
+        v.addWidget(self._global_force)
         self.setLayout(v)
 
     def set_ins_id(self, ins_id):
         self._ins_id = ins_id
         self._name.set_ins_id(ins_id)
+        self._global_force.set_ins_id(ins_id)
 
     def set_ui_model(self, ui_model):
         self._ui_model = ui_model
         self._name.set_ui_model(ui_model)
+        self._global_force.set_ui_model(ui_model)
 
     def unregister_updaters(self):
+        self._global_force.unregister_updaters()
         self._name.unregister_updaters()
 
 
