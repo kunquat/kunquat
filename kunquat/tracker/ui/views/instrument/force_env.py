1aeeeb65f1e56102c668cd278459301f9aebf002
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-10-25 15:08:34 +0300

    Add checkbox for enabling/disabling instrument force envelope.

--- kunquat/tracker/ui/views/instrument/force_env.py
+++ kunquat/tracker/ui/views/instrument/force_env.py
@@ -25,6 +25,11 @@ class ForceEnvelope(QWidget):
         self._ins_id = None
         self._updater = None
 
+        self._enabled_toggle = QCheckBox('Enabled')
+
+        h = QHBoxLayout()
+        h.addWidget(self._enabled_toggle)
+
         self._envelope = Envelope()
         self._envelope.set_node_count_max(32)
         self._envelope.set_y_range(0, 1)
@@ -33,6 +38,7 @@ class ForceEnvelope(QWidget):
         self._envelope.set_x_range_adjust(False, True)
 
         v = QVBoxLayout()
+        v.addLayout(h)
         v.addWidget(self._envelope)
         self.setLayout(v)
 
@@ -44,6 +50,10 @@ class ForceEnvelope(QWidget):
         self._updater = ui_model.get_updater()
         self._updater.register_updater(self._perform_updates)
         self._update_envelope()
+        QObject.connect(
+                self._enabled_toggle,
+                SIGNAL('stateChanged(int)'),
+                self._enabled_changed)
         QObject.connect(
                 self._envelope,
                 SIGNAL('envelopeChanged()'),
@@ -60,9 +70,27 @@ class ForceEnvelope(QWidget):
         module = self._ui_model.get_module()
         instrument = module.get_instrument(self._ins_id)
         envelope = instrument.get_force_envelope()
+
+        old_block = self._enabled_toggle.blockSignals(True)
+        self._enabled_toggle.setCheckState(
+                Qt.Checked if envelope['enabled'] else Qt.Unchecked)
+        self._enabled_toggle.blockSignals(old_block)
+
         self._envelope.set_nodes(envelope['envelope']['nodes'])
         self._envelope.set_loop_markers(envelope['envelope']['marks'])
         self._envelope.set_loop_enabled(envelope['loop'])
+
+    def _enabled_changed(self, state):
+        new_enabled = (self._enabled_toggle.checkState() == Qt.Checked)
+
+        module = self._ui_model.get_module()
+        instrument = module.get_instrument(self._ins_id)
+        envelope = instrument.get_force_envelope()
+
+        envelope['enabled'] = new_enabled
+
+        instrument.set_force_envelope(envelope)
+        self._updater.signal_update(set(['signal_instrument']))
 
     def _envelope_changed(self):
         new_nodes, new_loop = self._envelope.get_clear_changed()
