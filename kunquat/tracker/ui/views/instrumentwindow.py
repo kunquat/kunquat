1b72bc15bef04c2a72367dd4ce1da35a7664c483
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-11-29 22:08:05 +0200

    Show instrument name in instrument window title.

--- kunquat/tracker/ui/views/instrumentwindow.py
+++ kunquat/tracker/ui/views/instrumentwindow.py
@@ -23,6 +23,7 @@ class InstrumentWindow(QWidget):
         QWidget.__init__(self)
         self._ui_model = None
         self._ins_id = None
+        self._updater = None
         self._editor = Editor()
 
         v = QVBoxLayout()
@@ -36,10 +37,28 @@ class InstrumentWindow(QWidget):
     def set_ui_model(self, ui_model):
         assert self._ins_id != None
         self._ui_model = ui_model
+        self._updater = ui_model.get_updater()
+        self._updater.register_updater(self._perform_updates)
         self._editor.set_ui_model(ui_model)
+        self._update_title()
 
     def unregister_updaters(self):
         self._editor.unregister_updaters()
+        self._updater.unregister_updater(self._perform_updates)
+
+    def _perform_updates(self, signals):
+        if 'signal_controls' in signals:
+            self._update_title()
+
+    def _update_title(self):
+        module = self._ui_model.get_module()
+        instrument = module.get_instrument(self._ins_id)
+        name = instrument.get_name()
+        if name:
+            title = u'{} – Kunquat Tracker'.format(name)
+        else:
+            title = u'Kunquat Tracker'
+        self.setWindowTitle(title)
 
     def closeEvent(self, ev):
         ev.ignore()
