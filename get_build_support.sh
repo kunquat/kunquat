7d1c1b204f13b8a2b8872748e529fe07010f8896
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-12-01 22:24:26 +0200

    Check for existing local copy of Fabricate before downloading it.

--- get_build_support.sh
+++ get_build_support.sh
@@ -5,15 +5,34 @@ set -e
 
 verified_path="support/fabricate.py"
 unverified_path="support/fabricate_unverified.py"
-known_sha1sum_output="7510ec65a6135be611f1b311eb2851e74141859f  $unverified_path"
+known_sha1sum="7510ec65a6135be611f1b311eb2851e74141859f"
+
+
+# Check for existing verified version
+
+if [ -f $verified_path ]
+then
+    existing_sha1sum=$(sha1sum $verified_path | cut -d" " -f1)
+    if [ "$existing_sha1sum" = "$known_sha1sum" ]
+    then
+        echo ""
+        echo "Fabricate is already downloaded."
+        echo "You can build Kunquat by running ./make.py"
+        echo ""
+        exit 0
+    fi
+fi
+
+
+# Fabricate not found yet, so download it
 
 mkdir -p support
 touch support/__init__.py
 wget -O $unverified_path https://fabricate.googlecode.com/git/fabricate.py
 
-retrieved_sha1sum_output=$(sha1sum $unverified_path)
+retrieved_sha1sum=$(sha1sum $unverified_path | cut -d" " -f1)
 
-if [ "$retrieved_sha1sum_output" = "$known_sha1sum_output" ]
+if [ "$retrieved_sha1sum" = "$known_sha1sum" ]
 then
     echo "SHA1 checksum verified successfully."
     mv -v $unverified_path $verified_path
