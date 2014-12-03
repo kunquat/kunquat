a923b7b375b83b7317ff87884f8ea6f041cd02f2
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-12-03 18:35:49 +0200

    Update version numbers.

--- py-setup.py
+++ py-setup.py
@@ -49,7 +49,7 @@ else:
     sys.argv.remove('--disable-tracker')
 
 setup(name='kunquat',
-      version='0.6.1',
+      version='0.6.2',
       author='Tomi Jylhä-Ollila',
       author_email='tomi.jylha-ollila@iki.fi',
       url='http://kunquat.org/',
