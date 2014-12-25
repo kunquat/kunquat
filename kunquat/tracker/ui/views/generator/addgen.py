ca8a1be3648830ebc410483d0a46e9a775136b3d
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2014-12-25 21:47:33 +0200

    Add phase modulation volume slider to the additive generator editor.

--- kunquat/tracker/ui/views/generator/addgen.py
+++ kunquat/tracker/ui/views/generator/addgen.py
@@ -14,6 +14,8 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+from gennumslider import GenNumSlider
+
 
 class AddGen(QWidget):
 
@@ -25,21 +27,31 @@ class AddGen(QWidget):
         self._updater = None
 
         self._phase_mod_enabled_toggle = QCheckBox('Phase modulation')
+        self._phase_mod_volume = ModVolume()
+
+        self._phase_mod_container = QWidget()
+        pmc_layout = QVBoxLayout()
+        pmc_layout.addWidget(self._phase_mod_volume)
+        self._phase_mod_container.setLayout(pmc_layout)
 
         v = QVBoxLayout()
         v.addWidget(self._phase_mod_enabled_toggle)
+        v.addWidget(self._phase_mod_container)
         self.setLayout(v)
 
         self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
 
     def set_ins_id(self, ins_id):
         self._ins_id = ins_id
+        self._phase_mod_volume.set_ins_id(ins_id)
 
     def set_gen_id(self, gen_id):
         self._gen_id = gen_id
+        self._phase_mod_volume.set_gen_id(gen_id)
 
     def set_ui_model(self, ui_model):
         self._ui_model = ui_model
+        self._phase_mod_volume.set_ui_model(ui_model)
         self._updater = ui_model.get_updater()
         self._updater.register_updater(self._perform_updates)
         self._update_gen()
@@ -51,6 +63,7 @@ class AddGen(QWidget):
 
     def unregister_updaters(self):
         self._updater.unregister_updater(self._perform_updates)
+        self._phase_mod_volume.unregister_updaters()
 
     def _get_update_signal_type(self):
         return ''.join(('signal_gen_add_', self._ins_id, self._gen_id))
@@ -69,15 +82,45 @@ class AddGen(QWidget):
 
     def _update_gen(self):
         add_params = self._get_add_params()
+        phase_mod_enabled = add_params.get_phase_mod_enabled()
 
         old_block = self._phase_mod_enabled_toggle.blockSignals(True)
         self._phase_mod_enabled_toggle.setCheckState(
-                Qt.Checked if add_params.get_phase_mod_enabled() else Qt.Unchecked)
+                Qt.Checked if phase_mod_enabled else Qt.Unchecked)
         self._phase_mod_enabled_toggle.blockSignals(old_block)
+
+        self._phase_mod_container.setEnabled(phase_mod_enabled)
 
     def _phase_mod_enabled_changed(self, state):
         new_enabled = (state == Qt.Checked)
         add_params = self._get_add_params()
         add_params.set_phase_mod_enabled(new_enabled)
+        self._updater.signal_update(set([self._get_update_signal_type()]))
+
+
+class ModVolume(GenNumSlider):
+
+    def __init__(self):
+        GenNumSlider.__init__(self, 1, -64.0, 24.0)
+        self.set_number(0)
+
+    def _get_add_params(self):
+        module = self._ui_model.get_module()
+        instrument = module.get_instrument(self._ins_id)
+        generator = instrument.get_generator(self._gen_id)
+        add_params = generator.get_type_params()
+        return add_params
+
+    def _update_value(self):
+        add_params = self._get_add_params()
+        self.set_number(add_params.get_mod_volume())
+
+    def _value_changed(self, mod_volume):
+        add_params = self._get_add_params()
+        add_params.set_mod_volume(mod_volume)
+        self._updater.signal_update(set([self._get_update_signal_type()]))
+
+    def _get_update_signal_type(self):
+        return ''.join(('signal_add_mod_volume_', self._ins_id, self._gen_id))
 
 
