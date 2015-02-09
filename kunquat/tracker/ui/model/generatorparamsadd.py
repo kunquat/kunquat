1182a0a76f9408181146dae4cd1c64fa829e3a44
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-02-09 19:57:39 +0200

    Add additive generator base waveform tone support.

--- kunquat/tracker/ui/model/generatorparamsadd.py
+++ kunquat/tracker/ui/model/generatorparamsadd.py
@@ -120,6 +120,8 @@ class GeneratorParamsAdd(GeneratorParams):
         ]
     _POSTWARP_FUNCS_DICT = dict(_POSTWARP_FUNCS)
 
+    _TONES_MAX = 32
+
     def __init__(self, ins_id, gen_id, controller):
         GeneratorParams.__init__(self, ins_id, gen_id, controller)
 
@@ -268,6 +270,131 @@ class GeneratorParamsAdd(GeneratorParams):
         base_def = self._get_base_waveform_def()
         del base_def['postwarps'][index]
         self._update_base_waveform(base_def)
+
+    def _get_tone_subkey(self, index, subkey):
+        return 'tone_{:02x}/{}'.format(index, subkey)
+
+    def _get_tone_value(self, index, subkey, default_value):
+        tone_subkey = self._get_tone_subkey(index, subkey)
+        return self._get_value(tone_subkey, default_value)
+
+    def _set_tone_value(self, index, subkey, value):
+        tone_subkey = self._get_tone_subkey(index, subkey)
+        self._set_value(tone_subkey, value)
+
+    def _get_tone_pitch(self, index):
+        default_pitch = 1 if (index == 0) else 0
+        return self._get_tone_value(index, 'p_f_pitch.json', default_pitch)
+
+    def _set_tone_pitch(self, index, pitch):
+        self._set_tone_value(index, 'p_f_pitch.json', pitch)
+
+    def _get_tone_volume(self, index):
+        default_volume = 0 if (index == 0) else float('-inf')
+        return self._get_tone_value(index, 'p_f_volume.json', default_volume)
+
+    def _set_tone_volume(self, index, volume):
+        self._set_tone_value(index, 'p_f_volume.json', volume)
+
+    def _get_tone_panning(self, index):
+        return self._get_tone_value(index, 'p_f_pan.json', 0)
+
+    def _set_tone_panning(self, index, panning):
+        self._set_tone_value(index, 'p_f_pan.json', panning)
+
+    def _remove_tone(self, index):
+        if index == 0:
+            self._set_tone_pitch(index, 0)
+        else:
+            self._set_tone_pitch(index, None)
+
+        self._set_tone_volume(index, None)
+        self._set_tone_panning(index, None)
+
+    def _set_all_tones(self, tones):
+        # TODO: do this in one transaction
+        for i, tone in enumerate(tones):
+            pitch, volume, panning = tone
+            self._set_tone_pitch(i, pitch)
+            self._set_tone_volume(i, volume)
+            self._set_tone_panning(i, panning)
+        for i in xrange(len(tones), self._TONES_MAX):
+            self._remove_tone(i)
+
+    def _get_tone_existence(self, index):
+        has_pitch = (self._get_tone_pitch(index) > 0)
+        has_volume = not math.isinf(self._get_tone_volume(index))
+        return (has_pitch and has_volume)
+
+    def _get_tones_raw(self):
+        tones_raw = []
+        for i in xrange(self._TONES_MAX):
+            if self._get_tone_existence(i):
+                pitch = self._get_tone_pitch(i)
+                volume = self._get_tone_volume(i)
+                panning = self._get_tone_panning(i)
+                tones_raw.append((pitch, volume, panning))
+            else:
+                tones_raw.append(None)
+        return tones_raw
+
+    def _get_tones_and_packing_info(self):
+        tones = self._get_tones_raw()
+        has_holes = (None in tones) and (
+                tones.index(None) < sum(1 for t in tones if t != None))
+        tones = filter(lambda x: x != None, tones)
+        return tones, has_holes
+
+    def _get_tones(self):
+        tones, _ = self._get_tones_and_packing_info()
+        return tones
+
+    def get_tone_count(self):
+        return len(self._get_tones())
+
+    def add_tone(self):
+        tones, has_holes = self._get_tones_and_packing_info()
+        if has_holes:
+            tones.append([1, 0, 0])
+            self._set_all_tones(tones)
+        else:
+            new_index = len(tones)
+            self._set_tone_pitch(new_index, 1)
+            self._set_tone_volume(new_index, 0)
+            self._set_tone_panning(new_index, 0)
+
+    def get_tone_pitch(self, index):
+        return self._get_tones()[index][0]
+
+    def set_tone_pitch(self, index, pitch):
+        tones, has_holes = self._get_tones_and_packing_info()
+        if has_holes:
+            tones[index][0] = pitch
+            self._set_all_tones(tones)
+        else:
+            self._set_tone_pitch(index, pitch)
+
+    def get_tone_volume(self, index):
+        return self._get_tones()[index][1]
+
+    def set_tone_volume(self, index, volume):
+        tones, has_holes = self._get_tones_and_packing_info()
+        if has_holes:
+            tones[index][1] = volume
+            self._set_all_tones(tones)
+        else:
+            self._set_tone_volume(index, volume)
+
+    def get_tone_panning(self, index):
+        return self._get_tones()[index][2]
+
+    def set_tone_panning(self, index, panning):
+        tones, has_holes = self._get_tones_and_packing_info()
+        if has_holes:
+            tones[index][2] = panning
+            self._set_all_tones(tones)
+        else:
+            self._set_tone_panning(index, panning)
 
     def get_phase_mod_enabled(self):
         return (self._get_value('p_i_mod.json', 0) == 1)
