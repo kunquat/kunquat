9b5d1f52fc24b3a4ab43f0abe761024437e2aff9
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-02-21 00:12:50 +0200

    Remove asymmetric stretch prewarp function from the additive generator editor.

--- kunquat/tracker/ui/model/generatorparamsadd.py
+++ kunquat/tracker/ui/model/generatorparamsadd.py
@@ -73,12 +73,6 @@ def pre_stretch_inverse(x, amount):
         else:
             return -((-x)**(4**amount))
 
-def pre_stretch_asym(x, amount):
-    amount *= 2
-    x = (x + 1) / 2
-    x = x**(4**amount)
-    return x * 2 - 1
-
 def pre_scale(x, amount):
     return x * 8**amount
 
@@ -130,7 +124,6 @@ class GeneratorParamsAdd(GeneratorParams):
             ('Scale', pre_scale),
             ('Shift', pre_shift),
             ('Stretch', pre_stretch),
-            ('Stretch asym', pre_stretch_asym),
             ('Stretch inv', pre_stretch_inverse),
         ]
     _PREWARP_FUNCS_DICT = dict(_PREWARP_FUNCS)
