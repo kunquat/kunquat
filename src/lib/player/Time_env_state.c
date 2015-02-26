7a17445e57323e12a795dba353677513ed2446fd
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-02-26 22:11:08 +0200

    Add a state structure for processing envelopes with a time axis.

---
+++ src/lib/player/Time_env_state.c
@@ -0,0 +1,181 @@
+
+
+/*
+ * Author: Tomi Jylhä-Ollila, Finland 2015
+ *
+ * This file is part of Kunquat.
+ *
+ * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * To the extent possible under law, Kunquat Affirmers have waived all
+ * copyright and related or neighboring rights to Kunquat.
+ */
+
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <debug/assert.h>
+#include <devices/param_types/Envelope.h>
+#include <mathnum/common.h>
+#include <player/Time_env_state.h>
+#include <player/Work_buffers.h>
+
+
+void Time_env_state_init(Time_env_state* testate)
+{
+    assert(testate != NULL);
+
+    testate->is_finished = false;
+    testate->cur_pos = 0;
+    testate->next_node_index = 0;
+    testate->cur_value = NAN;
+    testate->update_value = 0;
+    testate->scale_factor = 1;
+
+    return;
+}
+
+
+int32_t Time_env_state_process(
+        Time_env_state* testate,
+        const Envelope* env,
+        double scale_amount,
+        double scale_center,
+        double min_value,
+        double max_value,
+        const Work_buffers* wbs,
+        int32_t buf_start,
+        int32_t buf_stop,
+        uint32_t audio_rate)
+{
+    assert(testate != NULL);
+    assert(env != NULL);
+    assert(wbs != NULL);
+    assert(buf_start >= 0);
+    assert(buf_stop >= 0);
+
+    const Work_buffer* wb_actual_pitches = Work_buffers_get_buffer(
+            wbs, WORK_BUFFER_ACTUAL_PITCHES);
+    const float* actual_pitches = Work_buffer_get_contents(wb_actual_pitches) + 1;
+
+    const Work_buffer* wb_time_env = Work_buffers_get_buffer(
+            wbs, WORK_BUFFER_TIME_ENV);
+    float* values = Work_buffer_get_contents_mut(wb_time_env) + 1;
+
+    // Get loop information
+    const int loop_start_index = Envelope_get_mark(env, 0);
+    const int loop_end_index = Envelope_get_mark(env, 1);
+    const double* loop_start =
+        (loop_start_index == -1) ? NULL : Envelope_get_node(env, loop_start_index);
+    const double* loop_end =
+        (loop_end_index == -1) ? NULL : Envelope_get_node(env, loop_end_index);
+
+    const bool has_loop = (loop_start != NULL) && (loop_end != NULL);
+
+    int32_t i = buf_start;
+    for (; i < buf_stop; ++i)
+    {
+        const float actual_pitch = actual_pitches[i];
+        const float prev_actual_pitch = actual_pitches[i - 1];
+
+        // Apply pitch-based scaling
+        if ((scale_amount != 0) && (actual_pitch != prev_actual_pitch))
+            testate->scale_factor = pow(
+                    actual_pitch / scale_center, scale_amount);
+
+        // Get envelope value at current position
+        double* next_node = Envelope_get_node(env, testate->next_node_index);
+        double value = NAN;
+
+        if (next_node == NULL)
+        {
+            const double* last_node = Envelope_get_node(
+                    env, Envelope_node_count(env) - 1);
+            value = last_node[1];
+        }
+        else if ((testate->cur_pos >= next_node[0]) || isnan(testate->update_value))
+        {
+            ++testate->next_node_index;
+            if ((loop_end_index >= 0) && (loop_end_index < testate->next_node_index))
+            {
+                assert(loop_start_index >= 0);
+                testate->next_node_index = loop_start_index;
+            }
+
+            value = Envelope_get_value(env, testate->cur_pos);
+
+            if (isfinite(value))
+            {
+                const double next_value = Envelope_get_value(
+                        env, testate->cur_pos + 1.0 / audio_rate);
+                testate->cur_value = value;
+                testate->update_value = next_value - value;
+            }
+            else
+            {
+                value = Envelope_get_node(env, Envelope_node_count(env) - 1)[1];
+            }
+        }
+        else
+        {
+            assert(isfinite(testate->update_value));
+            testate->cur_value += testate->update_value * testate->scale_factor;
+            value = clamp(testate->cur_value, min_value, max_value);
+        }
+
+        // Apply envelope value
+        assert(isfinite(value));
+        values[i] = value;
+
+        // Update envelope position
+        double new_pos = testate->cur_pos + testate->scale_factor / audio_rate;
+
+        if (has_loop)
+        {
+            // Handle loop
+            if (new_pos > loop_end[0])
+            {
+                const double loop_len = loop_end[0] - loop_start[0];
+                assert(loop_len >= 0);
+                if (loop_len == 0)
+                {
+                    new_pos = loop_end[0];
+                }
+                else
+                {
+                    const double exceed = new_pos - loop_end[0];
+                    const double offset = fmod(exceed, loop_len);
+                    new_pos = loop_start[0] + offset;
+                    assert(new_pos >= loop_start[0]);
+                    assert(new_pos <= loop_end[0]);
+
+                    // Following iteration will check if this index is too low,
+                    // so no need to find the exact result in a loop
+                    testate->next_node_index = loop_start_index;
+                }
+            }
+        }
+        else
+        {
+            // Check for end of envelope
+            double* last = Envelope_get_node(env, Envelope_node_count(env) - 1);
+            if (new_pos > last[0])
+            {
+                new_pos = last[0];
+                if ((testate->cur_pos > last[0]) && (last[1] == 0))
+                {
+                    testate->is_finished = true;
+                    break;
+                }
+            }
+        }
+
+        testate->cur_pos = new_pos;
+    }
+
+    return i;
+}
+
+
