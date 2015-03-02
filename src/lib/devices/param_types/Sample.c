f0ad7053933c2fbd0988ee60da4dc7c07f9e20ba
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-03-02 23:48:43 +0200

    Rewrite sample rendering.

--- src/lib/devices/param_types/Sample.c
+++ src/lib/devices/param_types/Sample.c
@@ -207,16 +207,263 @@ uint32_t Sample_mix(
     assert(tempo > 0);
     assert(vol_scale >= 0);
 
+    // This implementation does not support larger sample lengths :-P
+    assert(sample->len < INT32_MAX - 1);
+
+    if (sample->len == 0)
+    {
+        vstate->active = false;
+        return buf_start;
+    }
+
     const float* actual_pitches = Work_buffers_get_buffer_contents(
             wbs, WORK_BUFFER_ACTUAL_PITCHES);
     const float* actual_forces = Work_buffers_get_buffer_contents(
             wbs, WORK_BUFFER_ACTUAL_FORCES);
 
-    float* audio_l = Work_buffers_get_buffer_contents_mut(wbs, WORK_BUFFER_AUDIO_L);
-    float* audio_r = Work_buffers_get_buffer_contents_mut(wbs, WORK_BUFFER_AUDIO_R);
+    float* abufs[KQT_BUFFERS_MAX] =
+    {
+        Work_buffers_get_buffer_contents_mut(wbs, WORK_BUFFER_AUDIO_L),
+        Work_buffers_get_buffer_contents_mut(wbs, WORK_BUFFER_AUDIO_R),
+    };
 
-    uint64_t new_pos = vstate->pos;
+    static const int SAMPLE_WORK_BUFFER_POSITIONS = WORK_BUFFER_IMPL_1;
+    static const int SAMPLE_WORK_BUFFER_NEXT_POSITIONS = WORK_BUFFER_IMPL_2;
+    static const int SAMPLE_WORK_BUFFER_POSITIONS_REM = WORK_BUFFER_IMPL_3;
 
+    int32_t* positions = Work_buffers_get_buffer_contents_int_mut(
+            wbs, SAMPLE_WORK_BUFFER_POSITIONS);
+    int32_t* next_positions = Work_buffers_get_buffer_contents_int_mut(
+            wbs, SAMPLE_WORK_BUFFER_NEXT_POSITIONS);
+    float* positions_rem = Work_buffers_get_buffer_contents_mut(
+            wbs, SAMPLE_WORK_BUFFER_POSITIONS_REM);
+
+    int32_t new_pos = vstate->pos;
+    double new_pos_rem = vstate->pos_rem;
+
+    // Get sample positions (assuming no loop at this point)
+    const double shift_factor = middle_freq / (middle_tone * audio_rate);
+    positions[buf_start] = new_pos;
+    positions_rem[buf_start] = new_pos_rem;
+
+    for (int32_t i = buf_start; i < buf_stop; ++i)
+    {
+        const float actual_pitch = actual_pitches[i];
+        const double shift_total = actual_pitch * shift_factor;
+
+        const int32_t shift_floor = floor(shift_total);
+        const double shift_rem = shift_total - shift_floor;
+
+        new_pos += shift_floor;
+        new_pos_rem += shift_rem;
+        const int32_t excess_whole = floor(new_pos_rem);
+        new_pos += excess_whole;
+        new_pos_rem -= excess_whole;
+
+        positions[i + 1] = new_pos;
+        positions_rem[i + 1] = new_pos_rem;
+    }
+
+    // Apply loops and length constraints to sample positions
+    int32_t new_buf_stop = buf_stop;
+    switch (params->loop)
+    {
+        case SAMPLE_LOOP_OFF:
+        {
+            const int32_t length = (int32_t)sample->len;
+
+            // Current positions
+            for (int32_t i = buf_start; i < buf_stop; ++i)
+            {
+                if (positions[i] >= length)
+                {
+                    new_buf_stop = i;
+
+                    // Make the index safe to access
+                    positions[i] = length - 1;
+
+                    vstate->active = false;
+
+                    break;
+                }
+            }
+
+            // Next positions
+            for (int32_t i = buf_start; i < new_buf_stop; ++i)
+                next_positions[i] = positions[i] + 1;
+            next_positions[new_buf_stop] = min(length - 1, positions[new_buf_stop] + 1);
+        }
+        break;
+
+        case SAMPLE_LOOP_UNI:
+        {
+            const int32_t loop_start = params->loop_start;
+            const int32_t loop_end = params->loop_end;
+            const int32_t loop_length = max(1, loop_end - loop_start);
+
+            // Current positions
+            for (int32_t i = buf_start; i < buf_stop; ++i)
+            {
+                int32_t cur_pos = positions[i];
+                if (cur_pos > loop_start)
+                {
+                    int32_t loop_pos = cur_pos - loop_start;
+                    loop_pos %= loop_length;
+                    cur_pos = loop_start + loop_pos;
+                    positions[i] = cur_pos;
+                }
+            }
+
+            // Next positions
+            for (int32_t i = buf_start; i < buf_stop; ++i)
+            {
+                int32_t next_pos = positions[i] + 1;
+                if (next_pos >= loop_end)
+                    next_pos = loop_start;
+                next_positions[i] = next_pos;
+            }
+        }
+        break;
+
+        case SAMPLE_LOOP_BI:
+        {
+            const int32_t loop_start = params->loop_start;
+            const int32_t uni_loop_length = max(1, params->loop_end - loop_start) - 1;
+            const int32_t loop_length = max(1, uni_loop_length * 2);
+
+            for (int32_t i = buf_start; i < buf_stop; ++i)
+            {
+                int32_t cur_pos = positions[i];
+                if (cur_pos >= loop_start)
+                {
+                    // Current positions
+                    int32_t loop_pos = cur_pos - loop_start;
+                    loop_pos %= loop_length;
+                    if (loop_pos >= uni_loop_length)
+                        loop_pos = uni_loop_length - loop_pos;
+                    cur_pos = loop_start + loop_pos;
+                    positions[i] = cur_pos;
+
+                    // Next positions
+                    int32_t next_loop_pos = cur_pos + 1 - loop_start;
+                    next_loop_pos %= loop_length;
+                    if (next_loop_pos >= uni_loop_length)
+                        next_loop_pos = uni_loop_length - next_loop_pos;
+                    next_positions[i] = loop_start + next_loop_pos;
+                }
+            }
+        }
+        break;
+
+        default:
+            assert(false);
+    }
+
+    // Get sample frames
+#define get_item(out_value)                             \
+    if (true)                                           \
+    {                                                   \
+        const int32_t cur_pos = positions[i];           \
+        const int32_t next_pos = next_positions[i];     \
+        const float lerp_value = positions_rem[i];      \
+                                                        \
+        const float cur_value = data[cur_pos];          \
+        const float next_value = data[next_pos];        \
+        const float diff = next_value - cur_value;      \
+        (out_value) = cur_value + (lerp_value * diff);  \
+    }                                                   \
+    else (void)0
+
+    if (!sample->is_float)
+    {
+        switch (sample->bits)
+        {
+            case 8:
+            {
+                static const double scale = 1.0 / 0x80;
+                for (int ch = 0; ch < sample->channels; ++ch)
+                {
+                    const int8_t* data = sample->data[ch];
+                    float* audio_buffer = abufs[ch];
+
+                    for (int32_t i = buf_start; i < new_buf_stop; ++i)
+                    {
+                        const float actual_force = actual_forces[i];
+                        float item = 0;
+                        get_item(item);
+                        audio_buffer[i] = item * actual_force * scale;
+                    }
+                }
+            }
+            break;
+
+            case 16:
+            {
+                static const double scale = 1.0 / 0x8000UL;
+                for (int ch = 0; ch < sample->channels; ++ch)
+                {
+                    const int16_t* data = sample->data[ch];
+                    float* audio_buffer = abufs[ch];
+
+                    for (int32_t i = buf_start; i < new_buf_stop; ++i)
+                    {
+                        const float actual_force = actual_forces[i];
+                        float item = 0;
+                        get_item(item);
+                        audio_buffer[i] = item * actual_force * scale;
+                    }
+                }
+            }
+            break;
+
+            case 32:
+            {
+                static const double scale = 1.0 / 0x80000000UL;
+                for (int ch = 0; ch < sample->channels; ++ch)
+                {
+                    const int32_t* data = sample->data[ch];
+                    float* audio_buffer = abufs[ch];
+
+                    for (int32_t i = buf_start; i < new_buf_stop; ++i)
+                    {
+                        const float actual_force = actual_forces[i];
+                        float item = 0;
+                        get_item(item);
+                        audio_buffer[i] = item * actual_force * scale;
+                    }
+                }
+            }
+            break;
+
+            default:
+                assert(false);
+        }
+    }
+    else
+    {
+        for (int ch = 0; ch < sample->channels; ++ch)
+        {
+            const float* data = sample->data[ch];
+            float* audio_buffer = abufs[ch];
+
+            for (int32_t i = buf_start; i < new_buf_stop; ++i)
+            {
+                const float actual_force = actual_forces[i];
+                float item = 0;
+                get_item(item);
+                audio_buffer[i] = item * actual_force;
+            }
+        }
+    }
+
+#undef get_item
+
+    vstate->pos = new_pos;
+    vstate->pos_rem = new_pos_rem;
+
+    return new_buf_stop;
+
+#if 0
     int32_t mixed = buf_start;
     for (; mixed < buf_stop && vstate->active; ++mixed)
     {
@@ -432,6 +679,7 @@ uint32_t Sample_mix(
     vstate->pos = new_pos;
 
     return mixed;
+#endif
 }
 
 
