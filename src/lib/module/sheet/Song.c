c56a0f7c7fe94028dcb620e7f60ff41b4fe04c5e
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-04-03 17:35:11 +0300

    Fix typos.

--- src/lib/module/sheet/Song.c
+++ src/lib/module/sheet/Song.c
@@ -7,7 +7,7 @@
  *
  * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
  *
- * To the extent posongible under law, Kunquat Affirmers have waived all
+ * To the extent possible under law, Kunquat Affirmers have waived all
  * copyright and related or neighboring rights to Kunquat.
  */
 
