c95c56e0ccb860e53d67cde4675a1c956403d38c
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-04-06 18:09:26 +0300

    Add force->filter envelope editor.

--- kunquat/tracker/ui/views/audio_unit/filtereditor.py
+++ kunquat/tracker/ui/views/audio_unit/filtereditor.py
@@ -15,6 +15,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from kunquat.tracker.ui.views.envelope import Envelope
+from simple_env import SimpleEnvelope
 from time_env import TimeEnvelope
 
 
@@ -27,25 +28,35 @@ class FilterEditor(QWidget):
 
         self._filter_env = FilterEnvelope()
         self._filter_rel_env = FilterReleaseEnvelope()
+        self._force_filter_env = ForceFilterEnvelope()
+
+        b = QHBoxLayout()
+        b.setMargin(0)
+        b.setSpacing(16)
+        b.addWidget(self._filter_rel_env, 2)
+        b.addWidget(self._force_filter_env, 1)
 
         v = QVBoxLayout()
         v.setMargin(8)
-        v.setSpacing(8)
+        v.setSpacing(16)
         v.addWidget(self._filter_env)
-        v.addWidget(self._filter_rel_env)
+        v.addLayout(b)
         self.setLayout(v)
 
     def set_au_id(self, au_id):
         self._au_id = au_id
         self._filter_env.set_au_id(au_id)
         self._filter_rel_env.set_au_id(au_id)
+        self._force_filter_env.set_au_id(au_id)
 
     def set_ui_model(self, ui_model):
         self._ui_model = ui_model
         self._filter_env.set_ui_model(ui_model)
         self._filter_rel_env.set_ui_model(ui_model)
+        self._force_filter_env.set_ui_model(ui_model)
 
     def unregister_updaters(self):
+        self._force_filter_env.unregister_updaters()
         self._filter_rel_env.unregister_updaters()
         self._filter_env.unregister_updaters()
 
@@ -155,6 +166,44 @@ class FilterReleaseEnvelope(TimeEnvelope):
 
     def _set_envelope_data(self, envelope):
         self._get_audio_unit().set_filter_release_envelope(envelope)
+
+    def _get_audio_unit(self):
+        module = self._ui_model.get_module()
+        au = module.get_audio_unit(self._au_id)
+        return au
+
+
+class ForceFilterEnvelope(SimpleEnvelope):
+
+    def __init__(self):
+        SimpleEnvelope.__init__(self)
+
+    def _get_update_signal_type(self):
+        return '_'.join(('signal_force_filter_env', self._au_id))
+
+    def _get_title(self):
+        return 'Force->filter envelope'
+
+    def _make_envelope_widget(self):
+        envelope = Envelope({ 'is_square_area': True })
+        envelope.set_node_count_max(32)
+        envelope.set_y_range(0, 1)
+        envelope.set_x_range(0, 1)
+        envelope.set_first_lock(True, False)
+        envelope.set_last_lock(True, False)
+        return envelope
+
+    def _get_enabled(self):
+        return self._get_audio_unit().get_force_filter_envelope_enabled()
+
+    def _set_enabled(self, enabled):
+        self._get_audio_unit().set_force_filter_envelope_enabled(enabled)
+
+    def _get_envelope_data(self):
+        return self._get_audio_unit().get_force_filter_envelope()
+
+    def _set_envelope_data(self, envelope):
+        self._get_audio_unit().set_force_filter_envelope(envelope)
 
     def _get_audio_unit(self):
         module = self._ui_model.get_module()
