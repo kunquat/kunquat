749246f04956046bead2635ffeb1065a22d30bb5
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-05-17 10:45:36 +0300

    Add libkunquat limit constants to the Python interface.

---
+++ kunquat/kunquat/limits.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#
+# Authors: Tomi Jylhä-Ollila, Finland 2015
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
+
+from kunquat import get_limit_info
+
+
+"""Kunquat limit constants.
+
+"""
+
+
+for (limit_name, limit_value) in get_limit_info().iteritems():
+    globals()[limit_name] = limit_value
+
+
