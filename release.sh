6ea5db51e414c5a1d139a3f6eb270a95c5c94f9b
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-06-13 17:08:50 +0300

    Include correct version numbers when creating a release package.

--- release.sh
+++ release.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 
+set -e
+
 VERSION=0.6.2
 RELEASE=kunquat-$VERSION
 
 
+# Make version definitions for library and tracker
 echo "#define KQT_VERSION \"$VERSION\"" > src/include/kunquat/version_def.h
 echo "KUNQUAT_VERSION = '$VERSION'" > kunquat/tracker/version.py
 
-git archive -o $RELEASE.tar.gz --prefix=$RELEASE/ HEAD
+ver_stash=`git stash create`; git archive -o $RELEASE.tar.gz --prefix=$RELEASE/ ${ver_stash:-HEAD}
 
 exit 0
 
