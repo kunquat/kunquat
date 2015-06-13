b081d44516e149464990272177b77cb7eaff5504
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-06-13 20:03:57 +0300

    Verify working copy cleanliness before making a release package.

--- release.sh
+++ release.sh
@@ -1,17 +1,37 @@
 #!/bin/sh
 
 
-set -e
-
 VERSION=0.6.2
 RELEASE=kunquat-$VERSION
 
 
-# Make version definitions for library and tracker
-echo "#define KQT_VERSION \"$VERSION\"" > src/include/kunquat/version_def.h
-echo "KUNQUAT_VERSION = '$VERSION'" > kunquat/tracker/version.py
+set -e
 
+
+# Make sure there are no local changes
+# (required for the version substitution below to work correctly)
+if [ -n "$(git diff)" ] || [ -n "$(git diff --cached)" ]
+then
+    echo ""
+    echo "Cannot make a release package because of local changes."
+    echo ""
+    exit 1
+fi
+
+
+# Make version definitions for library and tracker
+version_lib_path="src/include/kunquat/version_def.h"
+version_tracker_path="kunquat/tracker/version.py"
+echo "#define KQT_VERSION \"$VERSION\"" > $version_lib_path
+echo "KUNQUAT_VERSION = '$VERSION'" > $version_tracker_path
+
+# Create release package
 ver_stash=`git stash create`; git archive -o $RELEASE.tar.gz --prefix=$RELEASE/ ${ver_stash:-HEAD}
+
+# Restore original version files from the repository
+git checkout $version_lib_path
+git checkout $version_tracker_path
+
 
 exit 0
 
