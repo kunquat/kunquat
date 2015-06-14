fa3315a0371ea3be2ad406cba8ba686a2276b508
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-06-14 22:39:57 +0300

    Tidy up layout of the About window and add author info.

--- kunquat/tracker/ui/views/about.py
+++ kunquat/tracker/ui/views/about.py
@@ -25,15 +25,18 @@ class AboutMessage(QWidget):
     def __init__(self):
         QWidget.__init__(self)
 
-        font_family = QFont().defaultFamily()
+        default_font = QFont()
+        default_family = default_font.defaultFamily()
+        default_size = default_font.pointSize()
 
         program_name = QLabel('Kunquat Tracker')
-        program_name.setFont(QFont('DejaVu Sans', 20, QFont.DemiBold))
+        program_name.setFont(QFont('DejaVu Sans', default_size + 9, QFont.DemiBold))
 
         tracker_version_str = 'Unreleased tracker version'
         if KUNQUAT_VERSION:
             tracker_version_str = 'Tracker version {}'.format(KUNQUAT_VERSION)
         tracker_version = QLabel(tracker_version_str)
+        tracker_version.setFont(QFont(default_family, default_size + 1, QFont.DemiBold))
 
         lib_version_str = 'Library version: {}'.format(get_version())
         lib_version = QLabel(lib_version_str)
@@ -45,10 +48,31 @@ class AboutMessage(QWidget):
         copyright_str = 'CC0 1.0 Universal'
         copyright = QLabel(copyright_str)
 
-        for label in (program_name, tracker_version, lib_version, website, copyright):
+        main_authors = QLabel('Main design and programming:')
+        main_authors.setFont(QFont(default_family, default_size, QFont.DemiBold))
+
+        main_authors_list_str = u'\n'.join((
+            u'Tomi Jylhä-Ollila',
+            u'Toni Ruottu',
+            ))
+        main_authors_list = QLabel(main_authors_list_str)
+
+        add_authors = QLabel('Additional contributors:')
+        add_authors.setFont(QFont(default_family, default_size, QFont.DemiBold))
+
+        add_authors_list_str = u'\n'.join((
+            u'Ossi Saresoja',
+            u'Sami Ketola',
+            ))
+        add_authors_list = QLabel(add_authors_list_str)
+
+        for label in (
+                program_name, tracker_version, lib_version, website, copyright,
+                main_authors, main_authors_list, add_authors, add_authors_list):
             label.setAlignment(Qt.AlignHCenter)
 
         v = QVBoxLayout()
+        v.setContentsMargins(8, 0, 8, 8)
         v.addWidget(program_name)
         v.addWidget(tracker_version)
         v.addWidget(lib_version)
@@ -56,6 +80,11 @@ class AboutMessage(QWidget):
         v.addWidget(website)
         v.addSpacing(8)
         v.addWidget(copyright)
+        v.addSpacing(8)
+        v.addWidget(main_authors)
+        v.addWidget(main_authors_list)
+        v.addWidget(add_authors)
+        v.addWidget(add_authors_list)
         v.addStretch(1)
         self.setLayout(v)
 
@@ -70,6 +99,8 @@ class About(QWidget):
         self._about_message = AboutMessage()
 
         v = QVBoxLayout()
+        v.setMargin(0)
+        v.setSpacing(0)
         v.setAlignment(Qt.AlignHCenter)
         v.addWidget(self._logo)
         v.setAlignment(self._logo, Qt.AlignHCenter)
