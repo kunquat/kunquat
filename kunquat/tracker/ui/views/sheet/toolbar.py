da4aa89b68534ad267998906ba6979e74b06a161
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-09-08 19:48:44 +0300

    Add pattern grid selector.

--- kunquat/tracker/ui/views/sheet/toolbar.py
+++ kunquat/tracker/ui/views/sheet/toolbar.py
@@ -43,6 +43,7 @@ class Toolbar(QWidget):
             ]
         self._grid_toggle = GridToggle()
         self._grid_editor_button = GridEditorButton()
+        self._grid_selector = GridSelector()
         self._length_editor = LengthEditor()
 
         h = QHBoxLayout()
@@ -66,6 +67,7 @@ class Toolbar(QWidget):
         spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
         h.addWidget(spacer)
 
+        h.addWidget(self._grid_selector)
         h.addWidget(self._length_editor)
 
         self.setLayout(h)
@@ -80,6 +82,7 @@ class Toolbar(QWidget):
             button.set_ui_model(ui_model)
         self._grid_toggle.set_ui_model(ui_model)
         self._grid_editor_button.set_ui_model(ui_model)
+        self._grid_selector.set_ui_model(ui_model)
         self._length_editor.set_ui_model(ui_model)
 
     def unregister_updaters(self):
@@ -91,6 +94,7 @@ class Toolbar(QWidget):
             button.unregister_updaters()
         self._grid_toggle.unregister_updaters()
         self._grid_editor_button.unregister_updaters()
+        self._grid_selector.unregister_updaters()
         self._length_editor.unregister_updaters()
 
 
@@ -171,6 +175,105 @@ class GridEditorButton(QToolButton):
     def _open_grid_editor(self):
         visibility_manager = self._ui_model.get_visibility_manager()
         visibility_manager.show_grid_editor()
+
+
+class GridSelector(QComboBox):
+
+    def __init__(self):
+        QComboBox.__init__(self)
+        self._ui_model = None
+        self._updater = None
+
+    def set_ui_model(self, ui_model):
+        self._ui_model = ui_model
+        self._updater = ui_model.get_updater()
+        self._updater.register_updater(self._perform_updates)
+
+        QObject.connect(
+                self, SIGNAL('currentIndexChanged(int)'), self._change_grid_pattern)
+
+    def unregister_updaters(self):
+        self._updater.unregister_updater(self._perform_updates)
+
+    def _update_grid_pattern_names(self):
+        grid_manager = self._ui_model.get_grid_manager()
+        gp_ids = grid_manager.get_grid_pattern_ids()
+
+        gp_items = [(gp_id, grid_manager.get_grid_pattern(gp_id).get_name())
+            for gp_id in gp_ids]
+        gp_items = sorted(gp_items, lambda x, y: cmp(x[1], y[1]))
+
+        old_block = self.blockSignals(True)
+        self.clear()
+        for (gp_id, name) in gp_items:
+            self.addItem(name, gp_id)
+        self.blockSignals(old_block)
+
+    def _get_pattern(self):
+        module = self._ui_model.get_module()
+        album = module.get_album()
+        if not album.get_existence():
+            return None
+
+        selection = self._ui_model.get_selection()
+        location = selection.get_location()
+        song = album.get_song_by_track(location.get_track())
+        pinst = song.get_pattern_instance(location.get_system())
+        pattern = pinst.get_pattern()
+        return pattern
+
+    def _set_default_grid_pattern(self):
+        grid_manager = self._ui_model.get_grid_manager()
+
+        gp_id = None
+
+        # Select the base grid of the current pattern if possible
+        pattern = self._get_pattern()
+        if pattern:
+            gp_id = pattern.get_base_grid_pattern_id()
+
+        # Fall back to whatever we can find from the grid pattern collection
+        if gp_id == None:
+            all_gp_ids = grid_manager.get_grid_pattern_ids()
+            if all_gp_ids:
+                gp_id = all_gp_ids[0]
+
+        if gp_id != None:
+            grid_manager.set_default_grid_pattern_id(gp_id)
+
+    def _update_grid_pattern_selection(self):
+        grid_manager = self._ui_model.get_grid_manager()
+        default_id = grid_manager.get_default_grid_pattern_id()
+
+        old_block = self.blockSignals(True)
+        index = self.findData(default_id)
+        assert index >= 0
+        self.setCurrentIndex(index)
+        self.blockSignals(old_block)
+
+    def _perform_updates(self, signals):
+        list_signals = set(['signal_module', 'signal_grid_pattern_list'])
+        if not signals.isdisjoint(list_signals):
+            self._update_grid_pattern_names()
+            self._set_default_grid_pattern()
+            self._update_grid_pattern_selection()
+
+        update_signals = set(['signal_module', 'signal_selection', 'signal_grid'])
+        if not signals.isdisjoint(update_signals):
+            self._set_default_grid_pattern()
+            self._update_grid_pattern_selection()
+
+    def _change_grid_pattern(self, index):
+        gp_id, success = self.itemData(index).toInt()
+        if (not success) or (gp_id == None):
+            return
+
+        pattern = self._get_pattern()
+        if not pattern:
+            return
+
+        pattern.set_base_grid_pattern_id(gp_id)
+        self._updater.signal_update(set(['signal_grid']))
 
 
 class HackSeparator(QFrame):
