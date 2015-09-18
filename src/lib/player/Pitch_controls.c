70204ad0a1995dfde8bffd7b6e7f22b4f57141eb
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-09-18 10:58:56 +0300

    Add pitch controls object to ease carrying of pitch parameters between notes.

---
+++ src/lib/player/Pitch_controls.c
@@ -0,0 +1,93 @@
+
+
+/*
+ * Author: Tomi Jylhä-Ollila, Finland 2015
+ *
+ * This file is part of Kunquat.
+ *
+ * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * To the extent possible under law, Kunquat Affirmers have waived all
+ * copyright and related or neighboring rights to Kunquat.
+ */
+
+
+#include <math.h>
+
+#include <debug/assert.h>
+#include <player/LFO.h>
+#include <player/Pitch_controls.h>
+#include <player/Slider.h>
+
+
+void Pitch_controls_init(Pitch_controls* pc, int32_t audio_rate, double tempo)
+{
+    assert(pc != NULL);
+    assert(audio_rate > 0);
+    assert(tempo > 0);
+
+    pc->pitch = NAN;
+    pc->orig_pitch = NAN;
+    pc->freq_mul = 1;
+    Pitch_controls_set_audio_rate(pc, audio_rate);
+    Pitch_controls_set_tempo(pc, tempo);
+
+    return;
+}
+
+
+void Pitch_controls_set_audio_rate(Pitch_controls* pc, int32_t audio_rate)
+{
+    assert(pc != NULL);
+    assert(audio_rate > 0);
+
+    Slider_set_mix_rate(&pc->slider, audio_rate);
+    LFO_set_mix_rate(&pc->vibrato, audio_rate);
+
+    return;
+}
+
+
+void Pitch_controls_set_tempo(Pitch_controls* pc, double tempo)
+{
+    assert(pc != NULL);
+    assert(tempo > 0);
+
+    Slider_set_tempo(&pc->slider, tempo);
+    LFO_set_tempo(&pc->vibrato, tempo);
+
+    return;
+}
+
+
+void Pitch_controls_reset(Pitch_controls* pc)
+{
+    assert(pc != NULL);
+
+    pc->pitch = NAN;
+    pc->orig_pitch = NAN;
+    pc->freq_mul = 1;
+    Slider_init(&pc->slider, SLIDE_MODE_EXP);
+    LFO_init(&pc->vibrato, LFO_MODE_EXP);
+
+    return;
+}
+
+
+void Pitch_controls_copy(
+        Pitch_controls* restrict dest, const Pitch_controls* restrict src)
+{
+    assert(dest != NULL);
+    assert(src != NULL);
+    assert(src != dest);
+
+    dest->pitch = src->pitch;
+    dest->orig_pitch = src->orig_pitch;
+    dest->freq_mul = src->freq_mul;
+    Slider_copy(&dest->slider, &src->slider);
+    LFO_copy(&dest->vibrato, &src->vibrato);
+
+    return;
+}
+
+
---
+++ src/lib/player/Pitch_controls.h
@@ -0,0 +1,87 @@
+
+
+/*
+ * Author: Tomi Jylhä-Ollila, Finland 2015
+ *
+ * This file is part of Kunquat.
+ *
+ * CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * To the extent possible under law, Kunquat Affirmers have waived all
+ * copyright and related or neighboring rights to Kunquat.
+ */
+
+
+#ifndef K_PITCH_CONTROLS_H
+#define K_PITCH_CONTROLS_H
+
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <player/LFO.h>
+#include <player/Slider.h>
+
+
+/**
+ * Pitch parameters that can be carried from one note to the next in a channel.
+ */
+typedef struct Pitch_controls
+{
+    double pitch;       // TODO: change this to cents
+    double orig_pitch;  // Note: this is in cents
+    double freq_mul;
+    Slider slider;
+    LFO    vibrato;
+} Pitch_controls;
+
+
+/**
+ * Initialise Pitch controls.
+ *
+ * \param pc           The Pitch controls -- must not be \c NULL.
+ * \param audio_rate   The audio rate -- must be positive.
+ * \param tempo        the tempo -- must be positive.
+ */
+void Pitch_controls_init(Pitch_controls* pc, int32_t audio_rate, double tempo);
+
+
+/**
+ * Set audio rate of the Pitch controls.
+ *
+ * \param pc           The Pitch controls -- must not be \c NULL.
+ * \param audio_rate   The audio rate -- must be positive.
+ */
+void Pitch_controls_set_audio_rate(Pitch_controls* pc, int32_t audio_rate);
+
+
+/**
+ * Set tempo of the Pitch controls.
+ *
+ * \param pc      The Pitch controls -- must not be \c NULL.
+ * \param tempo   the tempo -- must be positive.
+ */
+void Pitch_controls_set_tempo(Pitch_controls* pc, double tempo);
+
+
+/**
+ * Reset Pitch controls.
+ *
+ * \param pc   The Pitch controls -- must not be \c NULL.
+ */
+void Pitch_controls_reset(Pitch_controls* pc);
+
+
+/**
+ * Copy the current state of Pitch controls.
+ *
+ * \param dest   The destination Pitch controls -- must not be \c NULL.
+ * \param src    The source Pitch controls -- must not be \c NULL or \a dest.
+ */
+void Pitch_controls_copy(
+        Pitch_controls* restrict dest, const Pitch_controls* restrict src);
+
+
+#endif // K_PITCH_CONTROLS_H
+
+
