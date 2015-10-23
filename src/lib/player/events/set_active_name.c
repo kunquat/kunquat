05068b90b94de6ea476dd8a64ca0c9da984e79ac
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-10-23 15:18:46 +0300

    Add playback state space for active audio unit control variable names.

--- src/lib/player/events/set_active_name.c
+++ src/lib/player/events/set_active_name.c
@@ -30,8 +30,8 @@ bool set_active_name(
         const Value* value)
 {
     assert(gstate != NULL);
-    assert(cat < ACTIVE_CAT_LAST);
-    assert(type < ACTIVE_TYPE_LAST);
+    assert(cat < ACTIVE_CAT_COUNT);
+    assert(type < ACTIVE_TYPE_COUNT);
     assert(value != NULL);
     assert(value->type == VALUE_TYPE_STRING);
 
