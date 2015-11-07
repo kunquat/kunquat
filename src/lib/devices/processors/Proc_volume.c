b27aedc7b097afe426e35da410812c0e1d127797
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-11-07 19:37:28 +0200

    Add support for sliding volume processor volume with mixed signals.

--- src/lib/devices/processors/Proc_volume.c
+++ src/lib/devices/processors/Proc_volume.c
@@ -23,6 +23,8 @@
 #include <devices/Processor.h>
 #include <devices/processors/Proc_utils.h>
 #include <devices/processors/Proc_volume.h>
+#include <mathnum/conversions.h>
+#include <player/Linear_controls.h>
 #include <player/Proc_state.h>
 #include <string/common.h>
 #include <memory.h>
@@ -31,7 +33,7 @@
 typedef struct Volume_state
 {
     Proc_state parent;
-    double scale;
+    Linear_controls volume;
 } Volume_state;
 
 
@@ -45,6 +47,12 @@ typedef struct Proc_volume
 static Device_state* Proc_volume_create_state(
         const Device* device, int32_t audio_rate, int32_t audio_buffer_size);
 
+static bool Proc_volume_set_audio_rate(
+        const Device_impl* dimpl, Device_state* dstate, int32_t audio_rate);
+
+static void Proc_volume_update_tempo(
+        const Device_impl* dimpl, Device_state* dstate, double tempo);
+
 static void Proc_volume_reset(const Device_impl* dimpl, Device_state* dstate);
 
 static Set_float_func Proc_volume_set_volume;
@@ -52,6 +60,8 @@ static Set_float_func Proc_volume_set_volume;
 static Set_state_float_func Proc_volume_set_state_volume;
 
 static Set_cv_float_func Proc_volume_set_cv_volume;
+static Slide_target_cv_float_func Proc_volume_slide_target_cv_volume;
+static Slide_length_cv_float_func Proc_volume_slide_length_cv_volume;
 
 
 static bool Proc_volume_init(Device_impl* dimpl);
@@ -95,6 +105,8 @@ static bool Proc_volume_init(Device_impl* dimpl)
 
     Device_set_state_creator(volume->parent.device, Proc_volume_create_state);
 
+    Device_impl_register_set_audio_rate(&volume->parent, Proc_volume_set_audio_rate);
+    Device_impl_register_update_tempo(&volume->parent, Proc_volume_update_tempo);
     Device_impl_register_reset_device_state(&volume->parent, Proc_volume_reset);
 
     Processor* proc = (Processor*)volume->parent.device;
@@ -111,8 +123,12 @@ static bool Proc_volume_init(Device_impl* dimpl)
             Proc_volume_set_state_volume);
 
     reg_success &= Device_impl_register_updaters_cv_float(
-            &volume->parent, "v", Proc_volume_set_cv_volume,
-            NULL, NULL, NULL, NULL, NULL, NULL);
+            &volume->parent,
+            "v",
+            Proc_volume_set_cv_volume,
+            Proc_volume_slide_target_cv_volume,
+            Proc_volume_slide_length_cv_volume,
+            NULL, NULL, NULL, NULL);
 
     if (!reg_success)
         return false;
@@ -140,9 +156,40 @@ static Device_state* Proc_volume_create_state(
         return NULL;
     }
 
-    vol_state->scale = 1.0;
+    Linear_controls_init(&vol_state->volume, audio_rate, 120.0);
+    Linear_controls_set_value(&vol_state->volume, 0.0);
 
     return &vol_state->parent.parent;
+}
+
+
+static bool Proc_volume_set_audio_rate(
+        const Device_impl* dimpl, Device_state* dstate, int32_t audio_rate)
+{
+    assert(dimpl != NULL);
+    assert(dstate != NULL);
+    assert(audio_rate > 0);
+
+    Volume_state* vol_state = (Volume_state*)dstate;
+
+    Linear_controls_set_audio_rate(&vol_state->volume, audio_rate);
+
+    return true;
+}
+
+
+static void Proc_volume_update_tempo(
+        const Device_impl* dimpl, Device_state* dstate, double tempo)
+{
+    assert(dimpl != NULL);
+    assert(dstate != NULL);
+    assert(tempo > 0);
+
+    Volume_state* vol_state = (Volume_state*)dstate;
+
+    Linear_controls_set_tempo(&vol_state->volume, tempo);
+
+    return;
 }
 
 
@@ -156,17 +203,11 @@ static void Proc_volume_reset(const Device_impl* dimpl, Device_state* dstate)
     const double* vol_dB = Device_params_get_float(
             dimpl->device->dparams, "p_f_volume.json");
     if (vol_dB != NULL && isfinite(*vol_dB))
-        vol_state->scale = exp2(*vol_dB / 6);
+        Linear_controls_set_value(&vol_state->volume, *vol_dB);
     else
-        vol_state->scale = 1.0;
+        Linear_controls_set_value(&vol_state->volume, 0.0);
 
     return;
-}
-
-
-static double dB_to_scale(double vol_dB)
-{
-    return isfinite(vol_dB) ? exp2(vol_dB / 6) : 1.0;
 }
 
 
@@ -177,7 +218,7 @@ static bool Proc_volume_set_volume(
     assert(indices != NULL);
 
     Proc_volume* volume = (Proc_volume*)dimpl;
-    volume->scale = dB_to_scale(value);
+    volume->scale = isfinite(value) ? dB_to_scale(value) : 1.0;
 
     return true;
 }
@@ -208,9 +249,46 @@ static void Proc_volume_set_cv_volume(
     assert(dimpl != NULL);
     assert(dstate != NULL);
     assert(indices != NULL);
+    assert(isfinite(value));
 
     Volume_state* vol_state = (Volume_state*)dstate;
-    vol_state->scale = dB_to_scale(value);
+    Linear_controls_set_value(&vol_state->volume, value);
+
+    return;
+}
+
+
+static void Proc_volume_slide_target_cv_volume(
+        const Device_impl* dimpl,
+        Device_state* dstate,
+        Key_indices indices,
+        double value)
+{
+    assert(dimpl != NULL);
+    assert(dstate != NULL);
+    assert(indices != NULL);
+    assert(isfinite(value));
+
+    Volume_state* vol_state = (Volume_state*)dstate;
+    Linear_controls_slide_value_target(&vol_state->volume, value);
+
+    return;
+}
+
+
+static void Proc_volume_slide_length_cv_volume(
+        const Device_impl* dimpl,
+        Device_state* dstate,
+        Key_indices indices,
+        const Tstamp* length)
+{
+    assert(dimpl != NULL);
+    assert(dstate != NULL);
+    assert(indices != NULL);
+    assert(length != NULL);
+
+    Volume_state* vol_state = (Volume_state*)dstate;
+    Linear_controls_slide_value_length(&vol_state->volume, length);
 
     return;
 }
@@ -238,7 +316,7 @@ static uint32_t Proc_volume_process_vstate(
     assert(tempo > 0);
     assert(isfinite(tempo));
 
-    Volume_state* vol_state = (Volume_state*)proc_state;
+    const Proc_volume* vol = (const Proc_volume*)proc->parent.dimpl;
 
     // Get buffers
     Audio_buffer* in_buffer = Proc_state_get_voice_buffer_mut(
@@ -258,7 +336,7 @@ static uint32_t Proc_volume_process_vstate(
         const kqt_frame* in_values = Audio_buffer_get_buffer(in_buffer, ch);
         kqt_frame* out_values = Audio_buffer_get_buffer(out_buffer, ch);
 
-        const float scale = vol_state->scale;
+        const float scale = vol->scale;
 
         if (Processor_is_voice_feature_enabled(proc, 0, VOICE_FEATURE_FORCE))
         {
@@ -286,8 +364,8 @@ static void Proc_volume_process(
         const Device* device,
         Device_states* states,
         const Work_buffers* wbs,
-        uint32_t start,
-        uint32_t until,
+        uint32_t buf_start,
+        uint32_t buf_stop,
         uint32_t freq,
         double tempo)
 {
@@ -302,15 +380,26 @@ static void Proc_volume_process(
             states, Device_get_id(device));
     assert(vol_state != NULL);
 
+    // Update real-time control
+    static const int CONTROL_WORK_BUFFER_VOLUME = WORK_BUFFER_IMPL_1;
+
+    const Work_buffer* control_wb = Work_buffers_get_buffer(
+            wbs, CONTROL_WORK_BUFFER_VOLUME);
+    Linear_controls_fill_work_buffer(
+            &vol_state->volume, control_wb, buf_start, buf_stop);
+    const float* control_values = Work_buffer_get_contents(control_wb);
+
+    // Get port buffers
     kqt_frame* in_data[] = { NULL, NULL };
     kqt_frame* out_data[] = { NULL, NULL };
     get_raw_input(&vol_state->parent.parent, 0, in_data);
     get_raw_output(&vol_state->parent.parent, 0, out_data);
 
-    for (uint32_t frame = start; frame < until; ++frame)
+    for (uint32_t frame = buf_start; frame < buf_stop; ++frame)
     {
-        out_data[0][frame] += in_data[0][frame] * vol_state->scale;
-        out_data[1][frame] += in_data[1][frame] * vol_state->scale;
+        const float scale = dB_to_scale(control_values[frame]);
+        out_data[0][frame] += in_data[0][frame] * scale;
+        out_data[1][frame] += in_data[1][frame] * scale;
     }
 
     return;
