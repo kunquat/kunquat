c062223ca2510a0857f5942987e30dd5cbdd05fe
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-12-17 23:05:51 +0200

    Fix header include order in debugging modules.

--- src/lib/debug/assert.c
+++ src/lib/debug/assert.c
@@ -1,7 +1,7 @@
 
 
 /*
- * Author: Tomi Jylhä-Ollila, Finland 2010-2014
+ * Author: Tomi Jylhä-Ollila, Finland 2010-2015
  *
  * This file is part of Kunquat.
  *
@@ -12,11 +12,11 @@
  */
 
 
-#include <stdlib.h>
+#include <debug/assert.h>
+
 #include <stdbool.h>
 #include <stdio.h>
-
-#include <debug/assert.h>
+#include <stdlib.h>
 
 
 static bool is_printing_enabled = true;
