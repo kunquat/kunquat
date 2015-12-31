58b0cc30da4df555c42e4c7a5f420139b54bc295
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2015-12-31 18:55:10 +0200

    Rename control variable events.

--- src/lib/player/events/Event_au_types.h
+++ src/lib/player/events/Event_au_types.h
@@ -22,13 +22,13 @@ EVENT_AU_DEF("abp+",        bypass_on,                  NONE,       NULL)
 EVENT_AU_DEF("abp-",        bypass_off,                 NONE,       NULL)
 EVENT_AU_DEF("a.sus",       set_sustain,                FLOAT,      v_sustain)
 
-EVENT_AU_DEF("a.cv",        set_cv_value,               REALTIME,   v_finite_rt)
-EVENT_AU_DEF("a/cv",        slide_cv_target,            FLOAT,      v_finite_float)
-EVENT_AU_DEF("a/=cv",       slide_cv_length,            TSTAMP,     v_nonneg_ts)
-EVENT_AU_DEF("aoscv",       osc_speed_cv,               FLOAT,      v_nonneg_float)
-EVENT_AU_DEF("aodcv",       osc_depth_cv,               FLOAT,      v_nonneg_float)
-EVENT_AU_DEF("ao/=scv",     osc_speed_slide_cv,         TSTAMP,     v_nonneg_ts)
-EVENT_AU_DEF("ao/=dcv",     osc_depth_slide_cv,         TSTAMP,     v_nonneg_ts)
+EVENT_AU_DEF("a.v",         set_cv_value,               REALTIME,   v_finite_rt)
+EVENT_AU_DEF("a/v",         slide_cv_target,            FLOAT,      v_finite_float)
+EVENT_AU_DEF("a/=v",        slide_cv_length,            TSTAMP,     v_nonneg_ts)
+EVENT_AU_DEF("aosv",        osc_speed_cv,               FLOAT,      v_nonneg_float)
+EVENT_AU_DEF("aodv",        osc_depth_cv,               FLOAT,      v_nonneg_float)
+EVENT_AU_DEF("ao/=sv",      osc_speed_slide_cv,         TSTAMP,     v_nonneg_ts)
+EVENT_AU_DEF("ao/=dv",      osc_depth_slide_cv,         TSTAMP,     v_nonneg_ts)
 
 
 #undef EVENT_AU_DEF
