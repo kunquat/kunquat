124429fd4adbe8da6e08a4f3d56c8bf26bacadbe
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-01-09 23:32:56 +0200

    Remove obsolete code.

--- src/lib/player/devices/Voice_state_common.h
+++ src/lib/player/devices/Voice_state_common.h
@@ -49,66 +49,6 @@ void Voice_state_common_handle_pitch(
         int32_t buf_stop);
 
 
-#if 0
-/**
- * Process force.
- *
- * This function fills WORK_BUFFER_ACTUAL_FORCES with up-to-date values in the
- * specified buffer area. It must be called after \a Proc_common_handle_pitch.
- *
- * \param vstate        The Voice state -- must not be \c NULL.
- * \param au_state      The Audio unit state -- must not be \c NULL.
- * \param proc          The Processor -- must not be \c NULL.
- * \param wbs           The Work buffers -- must not be \c NULL.
- * \param audio_rate    The audio rate -- must be positive.
- * \param buf_start     The start index of the buffer area to be processed.
- * \param buf_stop      The stop index of the buffer area to be processed.
- *
- * \return   A new buffer stop index that is within the range of
- *           [\a buf_start, \a buf_stop]. Values less than \a buf_stop
- *           indicate that permanent silence is reached and the voice should be
- *           deactivated after the current process cycle.
- */
-int32_t Voice_state_common_handle_force(
-        Voice_state* vstate,
-        const Au_state* au_state,
-        const Processor* proc,
-        const Work_buffers* wbs,
-        uint32_t audio_rate,
-        int32_t buf_start,
-        int32_t buf_stop);
-
-
-/**
- * Process volume ramping at the end of a note.
- *
- * This function should be called after the process function of the processor
- * implementation.
- *
- * \param vstate          The Voice state -- must not be \c NULL.
- * \param proc_state      The Processor state -- must not be \c NULL.
- * \param au_state        The Audio unit state -- must not be \c NULL.
- * \param wbs             The Work buffers -- must not be \c NULL.
- * \param audio_rate      The audio rate -- must be positive.
- * \param buf_start       The start index of the buffer area to be processed.
- * \param buf_stop        The stop index of the buffer area to be processed.
- *
- * \return   A new buffer stop index that is within the range of
- *           [\a buf_start, \a buf_stop]. Values less than \a buf_stop
- *           indicate that permanent silence is reached and the voice should be
- *           deactivated after the current process cycle.
- */
-int32_t Voice_state_common_ramp_release(
-        Voice_state* vstate,
-        Proc_state* proc_state,
-        const Au_state* au_state,
-        const Work_buffers* wbs,
-        int32_t audio_rate,
-        int32_t buf_start,
-        int32_t buf_stop);
-#endif
-
-
 #endif // K_VOICE_STATE_COMMON_H
 
 
