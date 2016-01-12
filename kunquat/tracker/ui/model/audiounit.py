830949a4d4c2cbd9194a2c88d3046d9352dbafbf
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-01-12 19:46:31 +0200

    Create new volume processors with voice signals by default.

--- kunquat/tracker/ui/model/audiounit.py
+++ kunquat/tracker/ui/model/audiounit.py
@@ -41,7 +41,7 @@ _proc_defaults = {
     'ringmod':  { 'signal_type': u'mixed',
                   'ports': ['in_00', 'in_01', 'in_02', 'in_03', 'out_00', 'out_01'] },
     'sample':   { 'signal_type': u'voice', 'ports': ['out_00', 'out_01'] },
-    'volume':   { 'signal_type': u'mixed',
+    'volume':   { 'signal_type': u'voice',
                   'ports': ['in_00', 'in_01', 'in_02', 'out_00', 'out_01'] },
 }
 
