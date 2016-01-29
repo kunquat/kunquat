8a234a9b19332a5ac85c6ff30dc448d4f2be91b4
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-01-29 16:12:46 +0200

    Add faster version of the fast_exp2 function.

--- src/lib/mathnum/fast_exp2.h
+++ src/lib/mathnum/fast_exp2.h
@@ -1,7 +1,8 @@
 
 
 /*
- * Author: Tomi Jylhä-Ollila, Finland 2016
+ * Authors: Ossi Saresoja, Finland 2016
+ *          Tomi Jylhä-Ollila, Finland 2016
  *
  * This file is part of Kunquat.
  *
@@ -27,39 +28,49 @@
  *
  * \param x   The input value -- must be finite.
  *
- * \return   2 ^ \a x.
+ * \return   Roughly 2 ^ \a x.
  */
 inline double fast_exp2(double x)
 {
     assert(isfinite(x));
 
-    // TODO: Not that much faster than exp2 really, this needs more work!
+#define K 3
+#define N (1 << K)
+#define L 0.49278062009491144505781798
 
-#define KQT_FAST_EXP2_LN2   0.693147180559945309
-#define KQT_FAST_EXP2_LN2_2 0.480453013918201424
-#define KQT_FAST_EXP2_LN3_2 0.333024651988929479
-    static const double f2 = 0.5 * KQT_FAST_EXP2_LN2_2;
-    static const double f3 = (1.0 / 6.0) * KQT_FAST_EXP2_LN3_2;
+    static const double a[N] =
+    {
+        1.0000000000000000000000000,
+        1.0905077326652576592070107,
+        1.1892071150027210667175000,
+        1.2968395546510096659337541,
+        1.4142135623730950488016887,
+        1.5422108254079408236122919,
+        1.6817928305074290860622510,
+        1.8340080864093424634870832
+    };
 
-    // For all fast_exp2(x) > 0, 2048 shifts x to positive side to avoid stupid fmod
-    static const double pre_shift = 2048.0 + 0.5;
+    static const double b[N] =
+    {
+        0.0866433975699931636771540,
+        0.0944852950344677400302341,
+        0.1030369448582453432116499,
+        0.1123625851181203074735361,
+        0.1225322679335683989642377,
+        0.1336223856825675311641740,
+        0.1457162448440193058635239,
+        0.1589046917773470349548137
+    };
 
-    // Shift x to range [-0.5, 0.5)
-    const double a = x + pre_shift;
-    const double fa = floor(a);
-    const double sa = a - fa;
-    const double sx = sa - 0.5;
-    const int shift_amount = fa - 2048;
+    x *= N;
+    const double i = floor(x + L);
+    const int j = i;
+    const int k = j & (N - 1);
+    return ldexp(a[k] + b[k] * (x - i), j >> K);
 
-    const double sx2 = sx * sx;
-    const double sx3 = sx2 * sx;
-
-    const double esx = 1 + (sx * KQT_FAST_EXP2_LN2) + (sx2 * f2) + (sx3 * f3);
-#undef KQT_FAST_EXP2_LN2
-#undef KQT_FAST_EXP2_LN2_2
-#undef KQT_FAST_EXP2_LN3_2
-
-    return ldexp(esx, shift_amount);
+#undef K
+#undef N
+#undef L
 }
 
 
