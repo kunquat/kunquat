0ea5b45b4d9395b7d3da38312498de7336fbebce
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-01-31 22:27:10 +0200

    Convert old tap delay editor into a single-tap delay editor.

--- kunquat/tracker/ui/model/procparamsdelay.py
+++ kunquat/tracker/ui/model/procparamsdelay.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Author: Tomi Jylhä-Ollila, Finland 2015
+# Author: Tomi Jylhä-Ollila, Finland 2015-2016
 #
 # This file is part of Kunquat.
 #
@@ -18,13 +18,16 @@ from procparams import ProcParams
 
 class ProcParamsDelay(ProcParams):
 
-    _TAPS_MAX = 32
-
     def __init__(self, proc_id, controller):
         ProcParams.__init__(self, proc_id, controller)
 
-    def get_max_tap_count(self):
-        return self._TAPS_MAX
+    def get_port_names(self):
+        return {
+            'in_00':  u'audio L',
+            'in_01':  u'audio R',
+            'out_00': u'audio L',
+            'out_01': u'audio R',
+        }
 
     def get_max_delay(self):
         return self._get_value('p_f_max_delay.json', 2.0)
@@ -32,112 +35,10 @@ class ProcParamsDelay(ProcParams):
     def set_max_delay(self, value):
         self._set_value('p_f_max_delay.json', value)
 
-    def _get_tap_subkey(self, tap_index, subkey):
-        return 'tap_{:02x}/{}'.format(tap_index, subkey)
+    def get_init_delay(self):
+        return self._get_value('p_f_init_delay.json', 0.0)
 
-    def _get_tap_value(self, tap_index, subkey, default_value):
-        tap_subkey = self._get_tap_subkey(tap_index, subkey)
-        return self._get_value(tap_subkey, default_value)
-
-    def _set_tap_value(self, tap_index, subkey, value):
-        tap_subkey = self._get_tap_subkey(tap_index, subkey)
-        self._set_value(tap_subkey, value)
-
-    def _get_tap_existence(self, tap_index):
-        has_delay = (self._get_tap_delay(tap_index) >= 0)
-        has_volume = not math.isinf(self._get_tap_volume(tap_index))
-        return (has_delay and has_volume)
-
-    def _get_tap_delay(self, tap_index):
-        default_delay = -1.0
-        return self._get_tap_value(tap_index, 'p_f_delay.json', default_delay)
-
-    def _set_tap_delay(self, tap_index, delay):
-        self._set_tap_value(tap_index, 'p_f_delay.json', delay)
-
-    def _get_tap_volume(self, tap_index):
-        default_volume = 0.0
-        return self._get_tap_value(tap_index, 'p_f_volume.json', default_volume)
-
-    def _set_tap_volume(self, tap_index, volume):
-        self._set_tap_value(tap_index, 'p_f_volume.json', volume)
-
-    def _get_taps_raw(self):
-        taps_raw = []
-        for i in xrange(self._TAPS_MAX):
-            if self._get_tap_existence(i):
-                delay = self._get_tap_delay(i)
-                volume = self._get_tap_volume(i)
-                taps_raw.append([delay, volume])
-            else:
-                taps_raw.append(None)
-        return taps_raw
-
-    def _get_taps_and_packing_info(self):
-        taps = self._get_taps_raw()
-        has_holes = (None in taps) and (
-                taps.index(None) < sum(1 for t in taps if t != None))
-        taps = filter(lambda x: x != None, taps)
-        return taps, has_holes
-
-    def _get_taps(self):
-        taps, _ = self._get_taps_and_packing_info()
-        return taps
-
-    def get_tap_count(self):
-        return len(self._get_taps())
-
-    def _set_all_taps(self, taps):
-        # TODO: do this in one transaction
-        for i, tap in enumerate(taps):
-            delay, volume = tap
-            self._set_tap_delay(i, delay)
-            self._set_tap_volume(i, volume)
-        for i in xrange(len(taps), self._TAPS_MAX):
-            self._remove_tap(i)
-
-    def add_tap(self):
-        taps, has_holes = self._get_taps_and_packing_info()
-        if has_holes:
-            taps.append([0, 0])
-            self._set_all_taps(taps)
-        else:
-            new_index = len(taps)
-            self._set_tap_delay(new_index, 0)
-            self._set_tap_volume(new_index, 0)
-
-    def get_tap_delay(self, tap_index):
-        return self._get_taps()[tap_index][0]
-
-    def set_tap_delay(self, tap_index, delay):
-        taps, has_holes = self._get_taps_and_packing_info()
-        if has_holes:
-            taps[tap_index][0] = delay
-            self._set_all_taps(taps)
-        else:
-            self._set_tap_delay(tap_index, delay)
-
-    def get_tap_volume(self, tap_index):
-        return self._get_taps()[tap_index][1]
-
-    def set_tap_volume(self, tap_index, volume):
-        taps, has_holes = self._get_taps_and_packing_info()
-        if has_holes:
-            taps[tap_index][1] = volume
-            self._set_all_taps(taps)
-        else:
-            self._set_tap_volume(tap_index, volume)
-
-    def _remove_tap(self, tap_index):
-        self._set_tap_delay(tap_index, None)
-        self._set_tap_volume(tap_index, None)
-
-    def remove_tap(self, tap_index):
-        taps, has_holes = self._get_taps_and_packing_info()
-        if has_holes:
-            del taps[tap_index]
-            self._set_all_taps(taps)
-        else:
-            self._remove_tap(tap_index)
+    def set_init_delay(self, value):
+        self._set_value('p_f_init_delay.json', value)
 
 
--- kunquat/tracker/ui/views/processor/delayproc.py
+++ kunquat/tracker/ui/views/processor/delayproc.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Author: Tomi Jylhä-Ollila, Finland 2015
+# Author: Tomi Jylhä-Ollila, Finland 2015-2016
 #
 # This file is part of Kunquat.
 #
@@ -31,37 +31,43 @@ class DelayProc(QWidget):
 
     @staticmethod
     def get_name():
-        return u'Tap delay'
+        return u'Delay'
 
     def __init__(self):
         QWidget.__init__(self)
 
-        self._max_delay = MaxDelaySpin()
-        self._tap_list = TapList()
+        self._max_delay = MaxDelay()
+        self._init_delay = InitDelay()
+
+        gl = QGridLayout()
+        gl.addWidget(QLabel('Maximum delay:'), 0, 0)
+        gl.addWidget(self._max_delay, 0, 1)
+        gl.addWidget(QLabel('Initial delay:'), 1, 0)
+        gl.addWidget(self._init_delay, 1, 1)
 
         v = QVBoxLayout()
-        v.addWidget(self._max_delay)
-        v.addWidget(self._tap_list)
+        v.addLayout(gl)
+        v.addStretch(1)
         self.setLayout(v)
 
     def set_au_id(self, au_id):
         self._max_delay.set_au_id(au_id)
-        self._tap_list.set_au_id(au_id)
+        self._init_delay.set_au_id(au_id)
 
     def set_proc_id(self, proc_id):
         self._max_delay.set_proc_id(proc_id)
-        self._tap_list.set_proc_id(proc_id)
+        self._init_delay.set_proc_id(proc_id)
 
     def set_ui_model(self, ui_model):
         self._max_delay.set_ui_model(ui_model)
-        self._tap_list.set_ui_model(ui_model)
+        self._init_delay.set_ui_model(ui_model)
 
     def unregister_updaters(self):
-        self._tap_list.unregister_updaters()
+        self._init_delay.unregister_updaters()
         self._max_delay.unregister_updaters()
 
 
-class MaxDelaySpin(QWidget):
+class MaxDelay(QDoubleSpinBox):
 
     def __init__(self):
         QWidget.__init__(self)
@@ -70,20 +76,12 @@ class MaxDelaySpin(QWidget):
         self._ui_model = None
         self._updater = None
 
-        self._spin = QDoubleSpinBox()
-        self._spin.setDecimals(4)
-        self._spin.setMinimum(0.001)
-        self._spin.setMaximum(60)
-        self._spin.setValue(1)
+        self.setDecimals(4)
+        self.setMinimum(0.001)
+        self.setMaximum(60)
+        self.setValue(1)
 
-        h = QHBoxLayout()
-        h.setMargin(0)
-        h.addWidget(QLabel('Maximum delay'))
-        h.addWidget(self._spin)
-        h.addStretch()
-        self.setLayout(h)
-
-        self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Maximum)
+        #self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Maximum)
 
     def set_au_id(self, au_id):
         self._au_id = au_id
@@ -97,7 +95,7 @@ class MaxDelaySpin(QWidget):
         self._updater.register_updater(self._perform_updates)
 
         self._update_value()
-        QObject.connect(self._spin, SIGNAL('valueChanged(double)'), self._value_changed)
+        QObject.connect(self, SIGNAL('valueChanged(double)'), self._value_changed)
 
     def unregister_updaters(self):
         self._updater.unregister_updater(self._perform_updates)
@@ -112,11 +110,11 @@ class MaxDelaySpin(QWidget):
     def _update_value(self):
         delay_params = get_delay_params(self)
 
-        old_block = self._spin.blockSignals(True)
+        old_block = self.blockSignals(True)
         new_delay = delay_params.get_max_delay()
-        if new_delay != self._spin.value():
-            self._spin.setValue(new_delay)
-        self._spin.blockSignals(old_block)
+        if new_delay != self.value():
+            self.setValue(new_delay)
+        self.blockSignals(old_block)
 
     def _value_changed(self, value):
         delay_params = get_delay_params(self)
@@ -124,217 +122,20 @@ class MaxDelaySpin(QWidget):
         self._updater.signal_update(set([self._get_update_signal_type()]))
 
 
-class TapList(EditorList):
+class InitDelay(ProcNumSlider):
 
     def __init__(self):
-        EditorList.__init__(self)
-        self._au_id = None
-        self._proc_id = None
-        self._ui_model = None
-        self._updater = None
-        self._icon_bank = None
-
-        self._adder = None
-
-    def set_au_id(self, au_id):
-        self._au_id = au_id
-
-    def set_proc_id(self, proc_id):
-        self._proc_id = proc_id
-
-    def set_ui_model(self, ui_model):
-        self._ui_model = ui_model
-        self._icon_bank = ui_model.get_icon_bank()
-        self._updater = ui_model.get_updater()
-        self._updater.register_updater(self._perform_updates)
-        self._update_all()
-
-    def unregister_updaters(self):
-        self.disconnect_widgets()
-        self._updater.unregister_updater(self._perform_updates)
-
-    def _make_adder_widget(self):
-        self._adder = TapAdder()
-        self._adder.set_au_id(self._au_id)
-        self._adder.set_proc_id(self._proc_id)
-        self._adder.set_ui_model(self._ui_model)
-        return self._adder
-
-    def _get_updated_editor_count(self):
-        delay_params = get_delay_params(self)
-        tap_count = delay_params.get_tap_count()
-        return tap_count
-
-    def _make_editor_widget(self, index):
-        editor = TapEditor(index, self._icon_bank)
-        editor.set_au_id(self._au_id)
-        editor.set_proc_id(self._proc_id)
-        editor.set_ui_model(self._ui_model)
-        return editor
-
-    def _update_editor(self, index, editor):
-        pass
-
-    def _disconnect_widget(self, widget):
-        widget.unregister_updaters()
-
-    def _get_update_signal_type(self):
-        return '_'.join(('signal_proc_delay', self._proc_id))
-
-    def _perform_updates(self, signals):
-        if self._get_update_signal_type() in signals:
-            self._update_all()
-
-    def _update_all(self):
-        self.update_list()
-
-        delay_params = get_delay_params(self)
-        tap_count = delay_params.get_tap_count()
-        max_count_reached = (tap_count >= delay_params.get_max_tap_count())
-        self._adder.setVisible(not max_count_reached)
-
-
-class TapAdder(QPushButton):
-
-    def __init__(self):
-        QPushButton.__init__(self)
-        self._au_id = None
-        self._proc_id = None
-        self._ui_model = None
-        self._updater = None
-
-        self.setText('Add tap')
-
-    def set_au_id(self, au_id):
-        self._au_id = au_id
-
-    def set_proc_id(self, proc_id):
-        self._proc_id = proc_id
-
-    def set_ui_model(self, ui_model):
-        self._ui_model = ui_model
-        self._updater = ui_model.get_updater()
-
-        QObject.connect(self, SIGNAL('clicked()'), self._add_tap)
-
-    def unregister_updaters(self):
-        pass
-
-    def _get_update_signal_type(self):
-        return '_'.join(('signal_proc_delay', self._proc_id))
-
-    def _add_tap(self):
-        delay_params = get_delay_params(self)
-        delay_params.add_tap()
-        self._updater.signal_update(set([self._get_update_signal_type()]))
-
-
-class RemoveButton(QPushButton):
-
-    def __init__(self, icon):
-        QPushButton.__init__(self)
-        self.setStyleSheet('padding: 0 -2px;')
-        self.setIcon(QIcon(icon))
-
-
-class TapEditor(QWidget):
-
-    def __init__(self, tap_index, icon_bank):
-        QWidget.__init__(self)
-        self._au_id = None
-        self._proc_id = None
-        self._ui_model = None
-        self._updater = None
-        self._tap_index = tap_index
-
-        self._delay_slider = DelaySlider(tap_index)
-        self._volume_slider = VolumeSlider(tap_index)
-        self._remove_button = RemoveButton(icon_bank.get_icon_path('delete_small'))
-
-        h = QHBoxLayout()
-        h.setMargin(0)
-        h.setSpacing(2)
-        h.addWidget(self._delay_slider)
-        h.addWidget(self._volume_slider)
-        h.addWidget(self._remove_button)
-        self.setLayout(h)
-
-    def set_au_id(self, au_id):
-        self._au_id = au_id
-        self._delay_slider.set_au_id(au_id)
-        self._volume_slider.set_au_id(au_id)
-
-    def set_proc_id(self, proc_id):
-        self._proc_id = proc_id
-        self._delay_slider.set_proc_id(proc_id)
-        self._volume_slider.set_proc_id(proc_id)
-
-    def set_ui_model(self, ui_model):
-        self._delay_slider.set_ui_model(ui_model)
-        self._volume_slider.set_ui_model(ui_model)
-        self._ui_model = ui_model
-        self._updater = ui_model.get_updater()
-
-        QObject.connect(self._remove_button, SIGNAL('clicked()'), self._remove)
-
-    def unregister_updaters(self):
-        self._volume_slider.unregister_updaters()
-        self._delay_slider.unregister_updaters()
-
-    def _get_update_signal_type(self):
-        return '_'.join(('signal_proc_delay', self._proc_id))
-
-    def _remove(self):
-        delay_params = get_delay_params(self)
-        delay_params.remove_tap(self._tap_index)
-        self._updater.signal_update(set([self._get_update_signal_type()]))
-
-
-class DelaySlider(ProcNumSlider):
-
-    def __init__(self, tap_index):
-        ProcNumSlider.__init__(self, 4, 0, 60, title='Delay')
-        self._tap_index = tap_index
+        ProcNumSlider.__init__(self, 4, 0, 60, title='')
         self.set_number(0)
 
     def _update_value(self):
         delay_params = get_delay_params(self)
-
-        if self._tap_index >= delay_params.get_tap_count():
-            # We have been removed
-            return
-
         self.set_range(0, delay_params.get_max_delay())
-        self.set_number(delay_params.get_tap_delay(self._tap_index))
+        self.set_number(delay_params.get_init_delay())
 
     def _value_changed(self, value):
         delay_params = get_delay_params(self)
-        delay_params.set_tap_delay(self._tap_index, value)
-        self._updater.signal_update(set([self._get_update_signal_type()]))
-
-    def _get_update_signal_type(self):
-        return '_'.join(('signal_proc_delay', self._proc_id))
-
-
-class VolumeSlider(ProcNumSlider):
-
-    def __init__(self, tap_index):
-        ProcNumSlider.__init__(self, 1, -64.0, 18.0, title='Volume')
-        self._tap_index = tap_index
-        self.set_number(0)
-
-    def _update_value(self):
-        delay_params = get_delay_params(self)
-
-        if self._tap_index >= delay_params.get_tap_count():
-            # We have been removed
-            return
-
-        self.set_number(delay_params.get_tap_volume(self._tap_index))
-
-    def _value_changed(self, value):
-        delay_params = get_delay_params(self)
-        delay_params.set_tap_volume(self._tap_index, value)
+        delay_params.set_init_delay(value)
         self._updater.signal_update(set([self._get_update_signal_type()]))
 
     def _get_update_signal_type(self):
