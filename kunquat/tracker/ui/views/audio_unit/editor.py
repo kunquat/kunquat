a9fd3eea053a6d4dd4172a34b4a2ab1e3e8e143f
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-02-12 15:22:36 +0200

    Add instrument hit editor.

--- kunquat/tracker/ui/views/audio_unit/editor.py
+++ kunquat/tracker/ui/views/audio_unit/editor.py
@@ -14,8 +14,9 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-from infoeditor import InfoEditor
 from components import Components
+from hits import Hits
+from infoeditor import InfoEditor
 from testbutton import TestButton
 from kunquat.tracker.ui.views.keyboardmapper import KeyboardMapper
 
@@ -32,23 +33,16 @@ class Editor(QWidget):
         self._tabs = QTabWidget()
 
         self._components = Components()
+        self._hits = Hits()
         self._info_editor = InfoEditor()
-        self._tabs.addTab(self._components, 'Components')
-        self._tabs.addTab(self._info_editor, 'Info')
 
         self._keyboard_mapper = KeyboardMapper()
-
-        v = QVBoxLayout()
-        v.setMargin(4)
-        v.setSpacing(4)
-        v.addWidget(self._test_button)
-        v.addWidget(self._tabs)
-        self.setLayout(v)
 
     def set_au_id(self, au_id):
         self._au_id = au_id
         self._test_button.set_au_id(au_id)
         self._components.set_au_id(au_id)
+        self._hits.set_au_id(au_id)
         self._info_editor.set_au_id(au_id)
 
     def set_ui_model(self, ui_model):
@@ -56,15 +50,30 @@ class Editor(QWidget):
         self._control_manager = ui_model.get_control_manager()
         self._test_button.set_ui_model(ui_model)
         self._components.set_ui_model(ui_model)
+        self._hits.set_ui_model(ui_model)
         self._info_editor.set_ui_model(ui_model)
         self._keyboard_mapper.set_ui_model(ui_model)
 
         module = self._ui_model.get_module()
         au = module.get_audio_unit(self._au_id)
 
+        self._tabs.addTab(self._components, 'Components')
+        if au.is_instrument():
+            self._tabs.addTab(self._hits, 'Hits')
+        self._tabs.addTab(self._info_editor, 'Info')
+
+        v = QVBoxLayout()
+        v.setMargin(4)
+        v.setSpacing(4)
+        if au.is_instrument():
+            v.addWidget(self._test_button)
+        v.addWidget(self._tabs)
+        self.setLayout(v)
+
     def unregister_updaters(self):
         self._keyboard_mapper.unregister_updaters()
         self._info_editor.unregister_updaters()
+        self._hits.unregister_updaters()
         self._components.unregister_updaters()
         self._test_button.unregister_updaters()
 
