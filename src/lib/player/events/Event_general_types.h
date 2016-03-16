cc0d796fc7ca65e898e1f987db118c811e7d4112
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-03-16 22:32:29 +0200

    Update call events to match specification.

--- src/lib/player/events/Event_general_types.h
+++ src/lib/player/events/Event_general_types.h
@@ -1,7 +1,7 @@
 
 
 /*
- * Author: Tomi Jylhä-Ollila, Finland 2013-2014
+ * Author: Tomi Jylhä-Ollila, Finland 2013-2016
  *
  * This file is part of Kunquat.
  *
@@ -17,21 +17,17 @@
 #endif
 
 
-//                Name      Type suffix             Arg type        Validator
-EVENT_GENERAL_DEF("#",      comment,                STRING,         v_any_str)
+//                Name      Type suffix     Arg type        Validator
+EVENT_GENERAL_DEF("#",      comment,        STRING,         v_any_str)
 
-EVENT_GENERAL_DEF("?",      cond,                   BOOL,           v_any_bool)
-EVENT_GENERAL_DEF("?if",    if,                     NONE,           NULL)
-EVENT_GENERAL_DEF("?else",  else,                   NONE,           NULL)
-EVENT_GENERAL_DEF("?end",   end_if,                 NONE,           NULL)
+EVENT_GENERAL_DEF("?",      cond,           BOOL,           v_any_bool)
+EVENT_GENERAL_DEF("?if",    if,             NONE,           NULL)
+EVENT_GENERAL_DEF("?else",  else,           NONE,           NULL)
+EVENT_GENERAL_DEF("?end",   end_if,         NONE,           NULL)
 
-//EVENT_GENERAL_DEF("signal", signal,                 STRING,         v_any_str)
-//EVENT_GENERAL_DEF("callBn", call_bool_name,         STRING,         v_any_str)
-EVENT_GENERAL_DEF("callB",  call_bool,              BOOL,           v_any_bool)
-//EVENT_GENERAL_DEF("callIn", call_int_name,          STRING,         v_any_str)
-EVENT_GENERAL_DEF("callI",  call_int,               INT,            v_any_int)
-//EVENT_GENERAL_DEF"callFn",  call_float_name,        STRING,         v_any_str)
-EVENT_GENERAL_DEF("callF",  call_float,             FLOAT,          v_any_float)
+//EVENT_GENERAL_DEF("signal", signal,         STRING,         v_any_str)
+EVENT_GENERAL_DEF("calln",  call_name,      STRING,         v_any_str)
+EVENT_GENERAL_DEF("call",   call,           REALTIME,       NULL)
 
 
 #undef EVENT_GENERAL_DEF
