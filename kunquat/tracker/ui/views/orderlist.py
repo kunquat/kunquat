39857a22b98bfd44309dd84763cd661743d9850f
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-04-19 11:45:22 +0300

    Fix data deserialisation in order list.

--- kunquat/tracker/ui/views/orderlist.py
+++ kunquat/tracker/ui/views/orderlist.py
@@ -192,7 +192,7 @@ class AlbumTreeModel(QAbstractItemModel):
             return False
 
         data = mimedata.data('application/json')
-        items = json.loads(str(data, encoding='utf-8'))
+        items = json.loads(str(data))
         assert len(items) == 1
         item = items[0]
 
