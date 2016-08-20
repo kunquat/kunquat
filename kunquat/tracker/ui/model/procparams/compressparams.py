579399eb71bdf15606e2c86208ecba5444b70283
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2016-08-21 00:21:53 +0300

    Add compression controls to the compressor processor editor.

--- kunquat/tracker/ui/model/procparams/compressparams.py
+++ kunquat/tracker/ui/model/procparams/compressparams.py
@@ -30,7 +30,77 @@ class CompressParams(ProcParams):
             'out_02': 'gain',
         }
 
+    _DEFAULT_ATTACK = 1.0
+    _DEFAULT_RELEASE = 100.0
+
+    _DEFAULT_UPWARD_THRESHOLD = -60.0
+    _DEFAULT_DOWNWARD_THRESHOLD = 0.0
+    _DEFAULT_RATIO = 6.0
+
     def __init__(self, proc_id, controller):
         super().__init__(proc_id, controller)
+
+    @staticmethod
+    def get_react_range():
+        return (1.0, 2000.0)
+
+    def get_attack(self):
+        return self._get_value('p_f_attack.json', self._DEFAULT_ATTACK)
+
+    def set_attack(self, value):
+        self._set_value('p_f_attack.json', value)
+
+    def get_release(self):
+        return self._get_value('p_f_release.json', self._DEFAULT_RELEASE)
+
+    def set_release(self, value):
+        self._set_value('p_f_release.json', value)
+
+    @staticmethod
+    def get_threshold_range():
+        return (CompressParams._DEFAULT_UPWARD_THRESHOLD,
+                CompressParams._DEFAULT_DOWNWARD_THRESHOLD)
+
+    @staticmethod
+    def get_ratio_range():
+        return (1.0, 60.0)
+
+    def get_upward_enabled(self):
+        return self._get_value('p_b_upward_enabled.json', False)
+
+    def set_upward_enabled(self, enabled):
+        self._set_value('p_b_upward_enabled.json', enabled)
+
+    def get_upward_threshold(self):
+        return self._get_value(
+                'p_f_upward_threshold.json', self._DEFAULT_UPWARD_THRESHOLD)
+
+    def set_upward_threshold(self, value):
+        self._set_value('p_f_upward_threshold.json', value)
+
+    def get_upward_ratio(self):
+        return self._get_value('p_f_upward_ratio.json', self._DEFAULT_RATIO)
+
+    def set_upward_ratio(self, value):
+        self._set_value('p_f_upward_ratio.json', value)
+
+    def get_downward_enabled(self):
+        return self._get_value('p_b_downward_enabled.json', False)
+
+    def set_downward_enabled(self, enabled):
+        self._set_value('p_b_downward_enabled.json', enabled)
+
+    def get_downward_threshold(self):
+        return self._get_value(
+                'p_f_downward_threshold.json', self._DEFAULT_DOWNWARD_THRESHOLD)
+
+    def set_downward_threshold(self, value):
+        self._set_value('p_f_downward_threshold.json', value)
+
+    def get_downward_ratio(self):
+        return self._get_value('p_f_downward_ratio.json', self._DEFAULT_RATIO)
+
+    def set_downward_ratio(self, value):
+        self._set_value('p_f_downward_ratio.json', value)
 
 
--- kunquat/tracker/ui/views/processor/compressproc.py
+++ kunquat/tracker/ui/views/processor/compressproc.py
@@ -14,6 +14,11 @@
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+from kunquat.tracker.ui.model.procparams.compressparams import CompressParams
+from kunquat.tracker.ui.views.headerline import HeaderLine
+from .procnumslider import ProcNumSlider
+from . import utils
+
 
 class CompressProc(QWidget):
 
@@ -24,16 +29,219 @@ class CompressProc(QWidget):
     def __init__(self):
         super().__init__()
 
+        self._attack = Attack()
+        self._release = Release()
+
+        self._upward_config = CompressConfig('upward')
+        self._downward_config = CompressConfig('downward')
+
+        rl = QHBoxLayout()
+        rl.setContentsMargins(0, 0, 0, 0)
+        rl.setSpacing(10)
+        rl.addWidget(self._attack)
+        rl.addWidget(self._release)
+
+        v = QVBoxLayout()
+        v.setSpacing(10)
+        v.addLayout(rl)
+        v.addWidget(self._upward_config)
+        v.addWidget(self._downward_config)
+        v.addStretch(1)
+        self.setLayout(v)
+
     def set_au_id(self, au_id):
-        pass
+        self._attack.set_au_id(au_id)
+        self._release.set_au_id(au_id)
+        self._upward_config.set_au_id(au_id)
+        self._downward_config.set_au_id(au_id)
 
     def set_proc_id(self, proc_id):
-        pass
+        self._attack.set_proc_id(proc_id)
+        self._release.set_proc_id(proc_id)
+        self._upward_config.set_proc_id(proc_id)
+        self._downward_config.set_proc_id(proc_id)
 
     def set_ui_model(self, ui_model):
-        pass
+        self._attack.set_ui_model(ui_model)
+        self._release.set_ui_model(ui_model)
+        self._upward_config.set_ui_model(ui_model)
+        self._downward_config.set_ui_model(ui_model)
 
     def unregister_updaters(self):
-        pass
+        self._downward_config.unregister_updaters()
+        self._upward_config.unregister_updaters()
+        self._release.unregister_updaters()
+        self._attack.unregister_updaters()
+
+
+class CompressSlider(ProcNumSlider):
+
+    def __init__(self, decimals, min_value, max_value, title):
+        super().__init__(decimals, min_value, max_value, title)
+
+    def _get_compress_params(self):
+        return utils.get_proc_params(self._ui_model, self._au_id, self._proc_id)
+
+
+class Attack(CompressSlider):
+
+    def __init__(self):
+        min_value, max_value = CompressParams.get_react_range()
+        super().__init__(0, min_value, max_value, 'Attack:')
+
+    def _get_update_signal_type(self):
+        return 'signal_compress_attack_{}'.format(self._proc_id)
+
+    def _update_value(self):
+        params = self._get_compress_params()
+        self.set_number(params.get_attack())
+
+    def _value_changed(self, value):
+        params = self._get_compress_params()
+        params.set_attack(value)
+        self._updater.signal_update(set([self._get_update_signal_type()]))
+
+
+class Release(CompressSlider):
+
+    def __init__(self):
+        min_value, max_value = CompressParams.get_react_range()
+        super().__init__(0, min_value, max_value, 'Release:')
+
+    def _get_update_signal_type(self):
+        return 'signal_compress_release_{}'.format(self._proc_id)
+
+    def _update_value(self):
+        params = self._get_compress_params()
+        self.set_number(params.get_release())
+
+    def _value_changed(self, value):
+        params = self._get_compress_params()
+        params.set_release(value)
+        self._updater.signal_update(set([self._get_update_signal_type()]))
+
+
+class CompressConfig(QWidget):
+
+    def __init__(self, mode):
+        super().__init__()
+        self._au_id = None
+        self._proc_id = None
+        self._ui_model = None
+        self._updater = None
+
+        assert mode in ('upward', 'downward')
+        self._mode = mode
+
+        self._enabled = QCheckBox('Enabled')
+        self._threshold = Threshold(self._mode)
+        self._ratio = Ratio(self._mode)
+
+        pl = QHBoxLayout()
+        pl.setContentsMargins(0, 0, 0, 0)
+        pl.setSpacing(10)
+        pl.addWidget(self._enabled)
+        pl.addWidget(self._threshold)
+        pl.addWidget(self._ratio)
+
+        v = QVBoxLayout()
+        v.setContentsMargins(0, 0, 0, 0)
+        v.setSpacing(4)
+        v.addWidget(HeaderLine('{} compression'.format(mode.capitalize())))
+        v.addLayout(pl)
+        self.setLayout(v)
+
+    def set_au_id(self, au_id):
+        self._au_id = au_id
+        self._threshold.set_au_id(au_id)
+        self._ratio.set_au_id(au_id)
+
+    def set_proc_id(self, proc_id):
+        self._proc_id = proc_id
+        self._threshold.set_proc_id(proc_id)
+        self._ratio.set_proc_id(proc_id)
+
+    def set_ui_model(self, ui_model):
+        self._ui_model = ui_model
+        self._threshold.set_ui_model(ui_model)
+        self._ratio.set_ui_model(ui_model)
+        self._updater = ui_model.get_updater()
+        self._updater.register_updater(self._perform_updates)
+
+        QObject.connect(self._enabled, SIGNAL('stateChanged(int)'), self._change_enabled)
+
+        self._update_enabled()
+
+    def unregister_updaters(self):
+        self._updater.unregister_updater(self._perform_updates)
+        self._ratio.unregister_updaters()
+        self._threshold.unregister_updaters()
+
+    def _get_update_signal_type(self):
+        return 'signal_compress_enabled_{}'.format(self._proc_id)
+
+    def _perform_updates(self, signals):
+        if self._get_update_signal_type() in signals:
+            self._update_enabled()
+
+    def _get_compress_params(self):
+        return utils.get_proc_params(self._ui_model, self._au_id, self._proc_id)
+
+    def _update_enabled(self):
+        params = self._get_compress_params()
+        enabled = getattr(params, 'get_{}_enabled'.format(self._mode))()
+
+        old_block = self._enabled.blockSignals(True)
+        self._enabled.setCheckState(Qt.Checked if enabled else Qt.Unchecked)
+        self._enabled.blockSignals(old_block)
+
+        self._threshold.setEnabled(enabled)
+        self._ratio.setEnabled(enabled)
+
+    def _change_enabled(self, state):
+        enabled = (state == Qt.Checked)
+        params = self._get_compress_params()
+        getattr(params, 'set_{}_enabled'.format(self._mode))(enabled)
+        self._updater.signal_update(set([self._get_update_signal_type()]))
+
+
+class Threshold(CompressSlider):
+
+    def __init__(self, mode):
+        min_value, max_value = CompressParams.get_threshold_range()
+        super().__init__(1, min_value, max_value, 'Threshold:')
+        self._mode = mode
+
+    def _get_update_signal_type(self):
+        return 'signal_compress_{}_threshold_{}'.format(self._mode, self._proc_id)
+
+    def _update_value(self):
+        params = self._get_compress_params()
+        self.set_number(getattr(params, 'get_{}_threshold'.format(self._mode))())
+
+    def _value_changed(self, value):
+        params = self._get_compress_params()
+        getattr(params, 'set_{}_threshold'.format(self._mode))(value)
+        self._updater.signal_update(set([self._get_update_signal_type()]))
+
+
+class Ratio(CompressSlider):
+
+    def __init__(self, mode):
+        min_value, max_value = CompressParams.get_ratio_range()
+        super().__init__(1, min_value, max_value, 'Ratio:')
+        self._mode = mode
+
+    def _get_update_signal_type(self):
+        return 'signal_compress_{}_ratio_{}'.format(self._mode, self._proc_id)
+
+    def _update_value(self):
+        params = self._get_compress_params()
+        self.set_number(getattr(params, 'get_{}_ratio'.format(self._mode))())
+
+    def _value_changed(self, value):
+        params = self._get_compress_params()
+        getattr(params, 'set_{}_ratio'.format(self._mode))(value)
+        self._updater.signal_update(set([self._get_update_signal_type()]))
 
 
