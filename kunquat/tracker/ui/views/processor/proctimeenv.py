63ae3bdda48334b771d42b2b9253638dacaba2b6
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2017-02-04 08:17:33 +0200

    Add missing file.

---
+++ kunquat/tracker/ui/views/processor/proctimeenv.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#
+# Author: Tomi Jylhä-Ollila, Finland 2017
+#
+# This file is part of Kunquat.
+#
+# CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+#
+# To the extent possible under law, Kunquat Affirmers have waived all
+# copyright and related or neighboring rights to Kunquat.
+#
+
+from kunquat.tracker.ui.views.audiounit.autimeenv import AudioUnitTimeEnvelope
+from .processorupdater import ProcessorUpdater
+
+
+class ProcessorTimeEnvelope(AudioUnitTimeEnvelope, ProcessorUpdater):
+    pass
+
+
