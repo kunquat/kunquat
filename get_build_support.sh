777c88e4185f5befa713fb4054739d07054b5a7a
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2017-03-04 14:19:18 +0200

    Use SHA-256 instead of SHA-1 for checking the downloaded Fabricate script.

--- get_build_support.sh
+++ get_build_support.sh
@@ -3,12 +3,12 @@
 
 set -e
 
-# Specify location and SHA1 checksum of a tested Fabricate version
+# Specify location and SHA-256 checksum of a tested Fabricate version
 # Note: The current (2015-12-17) project home in GitHub does not contain an
 #       official release, so this URL refers to a specific commit instead.
 
 fabricate_url="https://raw.githubusercontent.com/SimonAlfie/fabricate/2bd38a581836e0bd7a3a5984de5b3354556fb42e/fabricate.py"
-known_sha1sum="fcd2b5da8dc46bbcf9747f63cc7a095947904eed"
+known_sha256sum="eb72f8904e62b1c85bfe69bdc3f6aba1a98581a5d83a5e674f9d7c522f172128"
 
 verified_path="support/fabricate.py"
 unverified_path="support/fabricate_unverified.py"
@@ -18,8 +18,8 @@ unverified_path="support/fabricate_unverified.py"
 
 if [ -f $verified_path ]
 then
-    existing_sha1sum=$(sha1sum $verified_path | cut -d" " -f1)
-    if [ "$existing_sha1sum" = "$known_sha1sum" ]
+    existing_sha256sum=$(sha256sum $verified_path | cut -d" " -f1)
+    if [ "$existing_sha256sum" = "$known_sha256sum" ]
     then
         echo ""
         echo "Fabricate is already downloaded."
@@ -36,18 +36,17 @@ mkdir -p support
 touch support/__init__.py
 wget -O $unverified_path $fabricate_url
 
-retrieved_sha1sum=$(sha1sum $unverified_path | cut -d" " -f1)
+retrieved_sha256sum=$(sha256sum $unverified_path | cut -d" " -f1)
 
-if [ "$retrieved_sha1sum" = "$known_sha1sum" ]
+if [ "$retrieved_sha256sum" = "$known_sha256sum" ]
 then
-    echo "SHA1 checksum verified successfully."
+    echo "SHA-256 checksum verified successfully."
     mv -v $unverified_path $verified_path
     echo "You can now build Kunquat by running ./make.py"
     echo ""
 else
-    echo "WARNING: Retrieved build support has unexpected contents."
-    echo "It is possible that Fabricate has been updated to a new version;"
-    echo "however, the downloaded file may also have been altered maliciously."
+    echo "WARNING: Retrieved build support has unexpected contents;"
+    echo "it is possible that the downloaded file has been altered maliciously."
     echo ""
     echo "The downloaded file is located at $unverified_path"
     echo "If you choose to trust this file, you can build Kunquat by running ./make.py --unsafe"
