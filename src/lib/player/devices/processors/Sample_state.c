cfc0a48fa9c407741861a90e4c10d66d314eaacf
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2017-05-14 22:50:50 +0300

    Deactivate voice states that receive final force of negative infinity.

--- src/lib/player/devices/processors/Sample_state.c
+++ src/lib/player/devices/processors/Sample_state.c
@@ -123,6 +123,16 @@ static int32_t Sample_render(
     Work_buffer* force_scales_wb = Device_thread_state_get_voice_buffer(
             proc_ts, DEVICE_PORT_TYPE_RECV, PORT_IN_FORCE);
     Work_buffer* dBs_wb = force_scales_wb;
+    if ((dBs_wb != NULL) &&
+            Work_buffer_is_final(dBs_wb) &&
+            (Work_buffer_get_const_start(dBs_wb) <= buf_start) &&
+            (Work_buffer_get_contents(dBs_wb)[buf_start] == -INFINITY))
+    {
+        // We are only getting silent force from this point onwards
+        vstate->active = false;
+        return buf_start;
+    }
+
     if (force_scales_wb == NULL)
         force_scales_wb = Work_buffers_get_buffer_mut(wbs, SAMPLE_WB_FIXED_FORCE);
     Proc_fill_scale_buffer(force_scales_wb, dBs_wb, buf_start, buf_stop);
