18959931759bb37e704d421a3fbc81947183f53d
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2017-07-21 13:02:35 +0300

    Fix character encoding in sheet clipboard operations.

--- kunquat/tracker/ui/views/sheet/utils.py
+++ kunquat/tracker/ui/views/sheet/utils.py
@@ -212,7 +212,7 @@ def is_clipboard_area_valid(sheet_manager):
     area_type = sheet_manager.get_serialised_area_type()
     if not mimedata.hasFormat(area_type):
         return False
-    area_data = str(mimedata.data(area_type))
+    area_data = str(mimedata.data(area_type), encoding='utf-8')
     return sheet_manager.is_area_data_valid(area_data)
 
 def try_paste_area(sheet_manager):
@@ -220,7 +220,7 @@ def try_paste_area(sheet_manager):
     mimedata = clipboard.mimeData()
     area_type = sheet_manager.get_serialised_area_type()
     if mimedata.hasFormat(area_type):
-        area_data = str(mimedata.data(area_type))
+        area_data = str(mimedata.data(area_type), encoding='utf-8')
         sheet_manager.try_paste_serialised_area(area_data)
 
 
