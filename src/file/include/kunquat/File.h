d85bcd88b958a6c8e6fd22fad8c9f1e4bca04a7f
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2018-04-22 16:59:24 +0300

    Add minimal documentation to the libkunquatfile interface header.

--- src/file/include/kunquat/File.h
+++ src/file/include/kunquat/File.h
@@ -12,17 +12,71 @@
  */
 
 
+#ifndef KQTFILE_FILE_H
+#define KQTFILE_FILE_H
+
+
 #include <kunquat/Handle.h>
 
 #include <stdlib.h>
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+ * \defgroup File Kunquat File Utilities
+ * \{
+ *
+ * \brief
+ * This module contains a simple interface for accessing Kunquat files.
+ *
+ * The header \c Handle.h contains an API for accessing Kunquat Handles.
+ */
+
+
+/**
+ * The identifier for accessing a Kunquat module.
+ */
 typedef int kqt_Module;
 
 
+/**
+ * Create a new Kunquat Handle from a Kunquat module.
+ *
+ * \param path   The path to an existing Kunquat module -- should be valid.
+ *
+ * \return   The new Kunquat Handle if successful. Otherwise, \c 0 is returned
+ *           and the Kunquat file error is set accordingly. Additionally, the
+ *           generic Kunquat Handle error may be set if the file contents were
+ *           invalid.
+ */
+kqt_Handle kqtfile_load_module(const char* path);
+
+
+/**
+ * Get human-readable error message from the Kunquat file interface.
+ *
+ * \param module   The Kunquat module, or \c 0 if retrieving error information
+ *                 that is not necessarily associated with an existing Kunquat
+ *                 module. For now, this should always be \c 0.
+ *
+ * \return   The latest error message. This is an empty string if no error has
+ *           occurred.
+ */
 const char* kqtfile_get_error(kqt_Module module);
 
 
-kqt_Handle kqtfile_load_module(const char* path);
+/* \} */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // KQTFILE_FILE_H
 
 
