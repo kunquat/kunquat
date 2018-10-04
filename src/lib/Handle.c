f1e0c0384aa52438e618304993da404a1ec68f53
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2018-10-04 22:01:41 +0300

    Move handle thread count accessors to the general handle module.

--- src/lib/Handle.c
+++ src/lib/Handle.c
@@ -123,6 +123,50 @@ kqt_Handle kqt_new_Handle(void)
 }
 
 
+int kqt_Handle_set_thread_count(kqt_Handle handle, int count)
+{
+    check_handle(handle, 0);
+
+    Handle* h = get_handle(handle);
+    check_data_is_valid(h, 0);
+    check_data_is_validated(h, 0);
+
+    if (count < 1)
+    {
+        Handle_set_error(h, ERROR_ARGUMENT, "Thread count must be positive");
+        return 0;
+    }
+    if (count > KQT_THREADS_MAX)
+    {
+        Handle_set_error(
+                h, ERROR_ARGUMENT, "Thread count must not exceed %d", KQT_THREADS_MAX);
+        return 0;
+    }
+
+    Error* error = ERROR_AUTO;
+
+    if (!Player_set_thread_count(h->player, count, error))
+    {
+        Handle_set_error_from_Error(h, error);
+        return 0;
+    }
+
+    return 1;
+}
+
+
+int kqt_Handle_get_thread_count(kqt_Handle handle)
+{
+    check_handle(handle, 0);
+
+    Handle* h = get_handle(handle);
+    check_data_is_valid(h, 0);
+    check_data_is_validated(h, 0);
+
+    return Player_get_thread_count(h->player);
+}
+
+
 int kqt_Handle_set_data(
         kqt_Handle handle, const char* key, const void* data, long length)
 {
