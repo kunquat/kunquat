1a72114579f2e7bac4cbfb5361eac785f8f50970
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2018-10-24 19:46:38 +0300

    Adjust envelope contents based on font size.

--- kunquat/tracker/ui/views/envelope.py
+++ kunquat/tracker/ui/views/envelope.py
@@ -11,6 +11,7 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
+from copy import deepcopy
 from itertools import count, islice
 import math
 import time
@@ -18,7 +19,7 @@ import time
 from kunquat.tracker.ui.qt import *
 
 from .axisrenderer import HorizontalAxisRenderer, VerticalAxisRenderer
-from .utils import lerp_val, set_glyph_rel_width
+from .utils import lerp_val, set_glyph_rel_width, get_scaled_font
 
 
 _font = QFont(QFont().defaultFamily(), 9, QFont.Bold)
@@ -567,22 +568,47 @@ class EnvelopeView(QWidget):
         disabled_colour = QColor(get_colour('bg_colour'))
         disabled_colour.setAlpha(0x7f)
 
+        font = get_scaled_font(style_mgr, 0.8, QFont.Bold)
+        set_glyph_rel_width(font, QWidget, '23456789' * 8, 50)
+
         config = {
-            'bg_colour': get_colour('envelope_bg_colour'),
-            'line_colour': get_colour('envelope_curve_colour'),
-            'node_colour': get_colour('envelope_node_colour'),
-            'focused_node_colour': focused_colour,
-            'focused_node_axis_colour': focused_axis_colour,
-            'loop_line_colour': get_colour('envelope_loop_marker_colour'),
-            'focused_loop_line_colour': focused_colour,
-            'loop_handle_colour': get_colour('envelope_loop_marker_colour'),
+            'padding'                   : style_mgr.get_scaled_size(0.5),
+            'bg_colour'                 : get_colour('envelope_bg_colour'),
+            'line_colour'               : get_colour('envelope_curve_colour'),
+            'node_colour'               : get_colour('envelope_node_colour'),
+            'focused_node_colour'       : focused_colour,
+            'focused_node_axis_colour'  : focused_axis_colour,
+            'node_size'                 : style_mgr.get_scaled_size(0.55),
+            'node_focus_dist_max'       : style_mgr.get_scaled_size(0.55),
+            'node_remove_dist_min'      : style_mgr.get_scaled_size(25),
+            'loop_line_colour'          : get_colour('envelope_loop_marker_colour'),
+            'focused_loop_line_colour'  : focused_colour,
+            'loop_line_dash'            : [style_mgr.get_scaled_size(0.4)] * 2,
+            'loop_handle_colour'        : get_colour('envelope_loop_marker_colour'),
             'focused_loop_handle_colour': focused_colour,
-            'disabled_colour': disabled_colour,
+            'loop_handle_size'          : style_mgr.get_scaled_size(1.3),
+            'loop_handle_focus_dist_max': style_mgr.get_scaled_size(1.5),
+            'disabled_colour'           : disabled_colour,
         }
 
         axis_config = {
-            'label_colour': get_colour('envelope_axis_label_colour'),
-            'line_colour': get_colour('envelope_axis_line_colour'),
+            'axis_x': {
+                'height'            : style_mgr.get_scaled_size(2),
+                'marker_min_dist'   : style_mgr.get_scaled_size(0.3),
+                'marker_min_width'  : style_mgr.get_scaled_size(0.3),
+                'marker_max_width'  : style_mgr.get_scaled_size(0.6),
+                'label_min_dist'    : style_mgr.get_scaled_size(10),
+            },
+            'axis_y': {
+                'width'             : style_mgr.get_scaled_size(5),
+                'marker_min_dist'   : style_mgr.get_scaled_size(0.3),
+                'marker_min_width'  : style_mgr.get_scaled_size(0.3),
+                'marker_max_width'  : style_mgr.get_scaled_size(0.6),
+                'label_min_dist'    : style_mgr.get_scaled_size(5),
+            },
+            'label_font'    : font,
+            'label_colour'  : get_colour('envelope_axis_label_colour'),
+            'line_colour'   : get_colour('envelope_axis_line_colour'),
         }
 
         self._set_configs(config, axis_config)
@@ -593,8 +619,17 @@ class EnvelopeView(QWidget):
         self._config.update(self._init_config)
         self._config.update(config)
 
-        self._axis_config = AXIS_CONFIG.copy()
+        axis_x = axis_config.pop('axis_x', {})
+        axis_y = axis_config.pop('axis_y', {})
+
+        def_label_font = AXIS_CONFIG.pop('label_font', None)
+        self._axis_config = deepcopy(AXIS_CONFIG)
+        self._axis_config['label_font'] = def_label_font
+        AXIS_CONFIG['label_font'] = def_label_font
+
         self._axis_config.update(axis_config)
+        self._axis_config['axis_x'].update(axis_x)
+        self._axis_config['axis_y'].update(axis_y)
         self._axis_x_renderer.set_config(self._axis_config, self)
         self._axis_y_renderer.set_config(self._axis_config, self)
 
