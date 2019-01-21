a6dd50d90ae86307833ef1ac462f8205e4fdb885
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2019-01-21 14:19:55 +0200

    Fix directory view layout.

--- kunquat/tracker/ui/views/filedialog.py
+++ kunquat/tracker/ui/views/filedialog.py
@@ -11,6 +11,8 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
+import datetime
+import os
 import os.path
 
 from kunquat.tracker.ui.qt import *
@@ -123,13 +125,73 @@ class DirectoryModel(QAbstractTableModel):
 
     def __init__(self):
         super().__init__()
+        self._current_dir = None
+        self._entries = []
+
+    def set_directory(self, new_dir):
+        self._current_dir = new_dir
+
+        self._entries = []
+
+        def entry_key(entry):
+            key = [entry[2], entry[0]]
+            if key[0] == 'Directory':
+                key[0] = ''
+            return key
+
+        with os.scandir(self._current_dir) as es:
+            for e in es:
+                is_dir = e.is_dir()
+                entry_type = 'Directory' if is_dir else 'File'
+                size = e.stat().st_size
+                modified = e.stat().st_mtime
+                new_entry = [e.name, size, entry_type, modified]
+                self._entries.append(new_entry)
+
+        self._entries.sort(key=entry_key)
+
+        parent_dir, _ = os.path.split(self._current_dir)
+        if parent_dir != self._current_dir:
+            pstat = os.stat(parent_dir)
+            parent_entry = ['..', pstat.st_size, 'Directory', pstat.st_mtime]
+            self._entries = [parent_entry] + self._entries
+
+        self._update_data()
+
+    def _update_data(self):
+        self.dataChanged.emit(
+                self.index(0, 0),
+                self.index(len(self._entries) - 1, len(self._HEADERS) - 1))
+
+    def _to_size_desc(self, byte_count):
+        if byte_count == 1:
+            return '1 byte'
+        elif byte_count < 1024:
+            return '{} bytes'.format(byte_count)
+
+        suffixes = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        mult = 1024
+        for s in suffixes[:-1]:
+            next_mult = mult * 1024
+            if mult <= byte_count < next_mult:
+                return '{:.1f} {}'.format(byte_count / mult, s)
+            mult = next_mult
+
+        return '{:.1f} {}'.format(byte_count / mult, suffixes[-1])
+
+    def _to_date(self, timestamp):
+        try:
+            dt = datetime.datetime.fromtimestamp(timestamp)
+        except (OverflowError, OSError):
+            return ''
+        return dt.isoformat(sep=' ', timespec='seconds')
 
     # Qt interface
 
     def rowCount(self, parent):
         if parent.isValid():
             return 0
-        return 0 # TODO: entry count
+        return len(self._entries)
 
     def columnCount(self, parent):
         if parent.isValid():
@@ -138,7 +200,17 @@ class DirectoryModel(QAbstractTableModel):
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-            return None # TODO
+            row, column = index.row(), index.column()
+            if 0 <= column < len(self._HEADERS) and 0 <= row < len(self._entries):
+                value = self._entries[row][column]
+                if column == 1:
+                    if self._entries[row][2] == 'Directory':
+                        value = ''
+                    else:
+                        value = self._to_size_desc(value)
+                elif column == 3:
+                    value = self._to_date(value)
+                return value
 
         return None
 
@@ -151,7 +223,7 @@ class DirectoryModel(QAbstractTableModel):
         return None
 
 
-class DirectoryView(QTableView):
+class DirectoryView(QTreeView):
 
     def __init__(self, ui_model, start_dir):
         super().__init__()
@@ -164,12 +236,14 @@ class DirectoryView(QTableView):
 
         self.setModel(self._model)
 
-        hh = self.horizontalHeader()
-        hh.setStretchLastSection(True)
-        hh.resizeSection(0, style_mgr.get_scaled_size(40))
-        hh.resizeSection(1, style_mgr.get_scaled_size(8))
-        hh.resizeSection(2, style_mgr.get_scaled_size(20))
-        hh.resizeSection(3, style_mgr.get_scaled_size(15))
+        header = self.header()
+        header.setStretchLastSection(True)
+        header.resizeSection(0, style_mgr.get_scaled_size(40))
+        header.resizeSection(1, style_mgr.get_scaled_size(9))
+        header.resizeSection(2, style_mgr.get_scaled_size(20))
+        header.resizeSection(3, style_mgr.get_scaled_size(15))
+
+        self._model.set_directory(self._current_dir)
 
     def get_current_dir(self):
         return self._current_dir
