27504079b70c76dc5566a86082900e7306d008f4
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2019-03-31 12:59:13 +0300

    Draw connection cables as smooth curves.

--- kunquat/tracker/ui/views/connectioncable.py
+++ kunquat/tracker/ui/views/connectioncable.py
@@ -11,30 +11,45 @@
 # copyright and related or neighboring rights to Kunquat.
 #
 
+import math
+
 from kunquat.tracker.ui.qt import *
 
 
 class ConnectionCable():
 
-    def __init__(self, from_point, to_point):
-        from_x, from_y = from_point
-        to_x, to_y = to_point
-        if from_x > to_x:
-            from_x, to_x = to_x, from_x
-            from_y, to_y = to_y, from_y
+    def __init__(self, start_point, end_point):
+        start_x, start_y = start_point
+        end_x, end_y = end_point
 
-        self._update_id = None
+        length_x = abs(end_x - start_x)
+        length_y = abs(end_y - start_y)
+        line_length = math.sqrt((length_x * length_x) + (length_y * length_y))
+        control_dist = line_length / 3
+        control_start_x = start_x + control_dist
+        control_end_x = end_x - control_dist
 
-        width = to_x - from_x + 1
-        height = abs(to_y - from_y) + 1
+        self._image = None
 
-        self._offset_x = from_x
-        self._offset_y = min(from_y, to_y)
+        min_x = min(start_x, end_x, control_end_x)
+        max_x = max(start_x, end_x, control_start_x)
+        min_y = min(start_y, end_y)
+        max_y = max(start_y, end_y)
 
-        self._is_ascending = from_y > to_y
+        self._start_offset_x = start_x - min_x
+        self._area_width = max_x - min_x
+        self._area_height = max_y - min_y
 
-        self._image = QImage(width, height, QImage.Format_ARGB32_Premultiplied)
-        self._image.fill(0)
+        self._start_point = QPointF(start_x - min_x, start_y - min_y)
+        self._control_start = QPointF(control_start_x - min_x, self._start_point.y())
+        self._mid_point = QPointF(
+                ((start_x + end_x) / 2) - min_x,
+                ((start_y + end_y) / 2) - min_y)
+        self._end_point = QPointF(end_x - min_x, end_y - min_y)
+        self._control_end = QPointF(control_end_x - min_x, self._end_point.y())
+
+        self._image_offset_x = start_x - self._start_point.x()
+        self._image_offset_y = min_y
 
         self._width = 1
         self._colour = None
@@ -46,8 +61,15 @@ class ConnectionCable():
         self._colour = colour
 
     def draw_line(self):
+        if not self._image:
+            width = self._area_width + self._width
+            height = self._area_height + self._width
+            self._image = QImage(width, height, QImage.Format_ARGB32_Premultiplied)
+            self._image.fill(0)
+
         painter = QPainter(self._image)
-        painter.translate(0.5, 0.5)
+        line_width_offset = math.floor((self._width - 1) / 2) + 0.5
+        painter.translate(line_width_offset, line_width_offset)
         painter.setRenderHint(QPainter.Antialiasing)
 
         # Test
@@ -55,26 +77,26 @@ class ConnectionCable():
         #painter.drawRect(0, 0, self._image.width() - 1, self._image.height() - 1)
 
         pen = QPen(self._colour)
-        pen.setWidth(self._width)
+        pen.setWidthF(self._width)
         painter.setPen(pen)
 
-        y1 = 0
-        y2 = self._image.height() - 1
-        if self._is_ascending:
-            y1, y2 = y2, y1
+        path = QPainterPath()
+        path.moveTo(self._start_point)
+        path.quadTo(self._control_start, self._mid_point)
+        path.quadTo(self._control_end, self._end_point)
 
-        painter.drawLine(0, y1, self._image.width() - 1, y2)
+        painter.drawPath(path)
+
+        painter.setPen(QColor(0, 0, 0))
+        painter.drawPoint(self._start_point)
+        painter.drawPoint(self._end_point)
 
     def copy_line(self, painter):
-        painter.save()
-        painter.translate(self._offset_x, self._offset_y)
-        painter.drawImage(0, 0, self._image)
-        painter.restore()
-
-    def set_update_id(self, update_id):
-        self._update_id = update_id
-
-    def get_update_id(self):
-        return self._update_id
+        line_width_offset = math.floor((self._width - 1) / 2) + 0.5
+        painter.drawImage(
+                QPointF(
+                    self._image_offset_x - line_width_offset,
+                    self._image_offset_y - line_width_offset),
+                self._image)
 
 
