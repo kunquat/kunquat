4ab2ee7608005583409856e378a66893af0a35bf
Tomi Jylhä-Ollila <tomi.jylha-ollila@iki.fi>
2019-06-21 20:38:13 +0300

    Remove buffer interleaving from the range map processor.

--- src/lib/player/devices/processors/Rangemap_state.h
+++ src/lib/player/devices/processors/Rangemap_state.h
@@ -1,7 +1,7 @@
 
 
 /*
- * Author: Tomi Jylhä-Ollila, Finland 2016-2018
+ * Author: Tomi Jylhä-Ollila, Finland 2016-2019
  *
  * This file is part of Kunquat.
  *
@@ -20,8 +20,6 @@
 #include <player/devices/Device_state.h>
 #include <player/devices/Voice_state.h>
 
-
-Device_impl_get_port_groups_func Rangemap_get_port_groups;
 
 Device_state_create_func new_Rangemap_pstate;
 
